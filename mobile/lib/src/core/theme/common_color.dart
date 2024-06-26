e535cc6fd7b667fecb44b9d2f8ac3df1c675fed9
serkhani <dak.boniface@gmail.com>
2024-06-26 07:03:38 +0000

    feat(mobile): added custom button

---
+++ mobile/lib/src/core/theme/common_color.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppCommonColors {
+  static const activeButton = Color(0xFF043174);
+  static const defaultLink = Color(0xFF007DFC);
+  static const hoverButton = Color(0xFFC3922E);
+  static const incorrectTextColor = Color(0xFFDE1F1F);
+  static const mainBlueButton = Color(0xFF1779F3);
+  static const red = Color(0xFFFC0000);
+  static const signInWithGoogleBgnd = Color(0xFF61677D);
+  static const logoGold = Color(0xFFC3922E);
+}
--- mobile/lib/src/core/theme/text_theme.dart
+++ mobile/lib/src/core/theme/text_theme.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/gen/fonts.gen.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 
+class CustomTextStyles {
+  static TextTheme kDefaultTextTheme(Color textColor) => TextTheme(
+        bodyMedium: TextStyle(
+          color: textColor,
+          fontSize: 16.sp,
+          fontWeight: FontWeight.w400,
+          fontFamily: FontFamily.nunito,
+        ),
+      );
+}
