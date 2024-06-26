ca534f2035265602eb65b9df69f7e254b89f56fb
Farad <alwaasiua@gmail.com>
2024-06-26 22:13:38 +0000

    google sign in

---
+++ mobile/lib/src/features/auth/presentation/widgets/sign_in_with_google.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/gen/assets.gen.dart';
+import 'package:mobile/src/core/theme/common_color.dart';
+import 'package:mobile/src/core/utils/utils.dart';
+import 'package:mobile/src/core/widgets/custom_button.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+
+class SignInWithGoogleBtn extends StatelessWidget {
+  final void Function() onPressed;
+  const SignInWithGoogleBtn({super.key, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomButton(
+      text: 'Sign In with Google',
+      onPressed: onPressed,
+      showSuffixWidget: true,
+      suffixWidget: Assets.svg.google.asSvgImage().rightPadding(16.0),
+      backgroundColor: AppCommonColors.signInWithGoogleBgnd.withOpacity(0.3),
+      borderColor: AppCommonColors.mainBlueButton,
+      horizontalPadding: 20.w,
+      textColor: context.colorScheme.onPrimary,
+      borderRadius: 14,
+    );
+  }
+}
