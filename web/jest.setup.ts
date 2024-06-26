dd13ad9e700651b3296ece4d2bd8fe71df07e13a
MIYAKA <75727824+BambinoRiya@users.noreply.github.com>
2024-06-26 15:09:57 +0000

    otp page ui

--- web/jest.setup.ts
+++
@@ -1,8 +0,0 @@
-import "@testing-library/jest-dom";
-import { TextEncoder } from "util";
-const { Response, Headers, Request } = require("whatwg-fetch");
-
-global.Response = Response;
-global.Headers = Headers;
-global.Request = Request;
-global.TextEncoder = TextEncoder;
