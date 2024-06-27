d1427faba58acab5811608da920f6db7201ffa4f
Michael Adu-Gyamfi <119356822+mikkayadu@users.noreply.github.com>
2024-06-27 14:35:31 +0000

    Ggpp 128 create db context and local database dependency (#10)
    * fix(backend): Generate a new Guid for id
    
    * feat(backend):Add packages to the Persistence and WebApi project
    
    * feat(backend): Register DbContext
    
    * feat(backend): Implement AppDbContext and AppDbContextFactory

--- backend/Domain/Common/BaseEntity.cs
+++ backend/Domain/Common/BaseEntity.cs
@@ -2,7 +2,7 @@ namespace Domain.Common
 {
     public class BaseEntity
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; set; }= Guid.NewGuid();
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
     }
---
+++ backend/Persistence/AppDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Persistence;
+using System.IO;
+
+public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var apiProjectPath = Path.Combine(Directory.GetCurrentDirectory(), "../WebApi");
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(apiProjectPath)
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var builder = new DbContextOptionsBuilder<AppDbContext>();
+        var connectionString = configuration.GetConnectionString("AppConnectionString");
+        builder.UseNpgsql(connectionString);
+
+        return new AppDbContext(builder.Options);
+    }
+}
+
