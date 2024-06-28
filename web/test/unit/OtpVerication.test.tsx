95cba2ffffa7a56a24f8425f50933d5d105a1d0a
lit <103055119+EricaAnnor@users.noreply.github.com>
2024-06-28 15:59:27 +0000

    feat(web): Design otp verification page

---
+++ web/test/unit/OtpVerication.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import OtpVerification from "@/components/OtpVerification";
+
+describe("Header", () => {
+  it("renders the submit button", () => {
+    render(<OtpVerification />);
+    const submitButton = screen.getByRole("button", { name: "Submit" });
+    expect(submitButton).toBeInTheDocument();
+  });
+
+  it("renders OTP Verification ", () => {
+    render(<OtpVerification />);
+    expect(screen.getByText("OTP Verification")).toBeInTheDocument();
+  });
+
+  it("renders exactly 2 images", () => {
+    render(<OtpVerification />);
+    const images = screen.getAllByRole("img");
+    expect(images.length).toBe(2);
+  });
+});
