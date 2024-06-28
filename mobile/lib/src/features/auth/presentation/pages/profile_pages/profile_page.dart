bad2a70ebcbb0c5c8b5424453a3690840d5e845b
FelixNotter <shepherdeinstein@gmail.com>
2024-06-28 00:49:11 +0000

    rebased main

---
+++ mobile/lib/src/features/auth/presentation/pages/profile_pages/profile_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:mobile/src/features/auth/presentation/widgets/profile_widgets/profile_cards.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+   
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+       
+        leading: IconButton(
+          icon: const Icon(CupertinoIcons.bell,
+         ),
+          onPressed: () {
+          },
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(CupertinoIcons.timer),
+            onPressed: () {
+            },
+          ),
+          IconButton(
+            icon: const Icon(CupertinoIcons.drop),
+            onPressed: () {
+            },
+          )
+        ],
+      ),
+      body: const Center(
+        child:  Column(
+         
+          children: [
+            Card1(),
+            SizedBox(height: 20),
+            Card2(),
+            SizedBox(height: 20),
+            Card3(),
+          ],
+        ),
+      ),
+    bottomNavigationBar: 
+    BottomNavigationBar(
+      items: 
+    [
+      BottomNavigationBarItem(icon: Icon(Icons.search),
+      label: "Explore" ),
+      BottomNavigationBarItem(icon: Icon(CupertinoIcons.heart),
+      label: "Wishlist"),
+      BottomNavigationBarItem(icon: Icon(CupertinoIcons.mail),
+      label: "Inbox"),
+    //  BottomNavigationBarItem(icon: Icon(CupertinoIcons.profile_circled),
+    //   label: "Profile"), 
+    ]
+    ),
+    );
+  }
+}
