daa0827e8729d6832ca77f93d97c43d3c940d2f1
Tariq Nasser Deen <tariqnassiru@gmail.com>
2024-06-29 14:41:08 +0000

    feat(password reset): Updated the screen inplementation to match the UI

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/src/core/theme/theme_config.dart';
 
+
 import 'package:mobile/src/features/auth/presentation/pages/otp_page.dart';
 
-import 'package:mobile/src/features/auth/presentation/otp.dart';
+
+
 import 'package:mobile/src/features/auth/presentation/password_reset.dart';
 
 import 'package:responsive_sizer/responsive_sizer.dart';
-import 'package:responsive_sizer/responsive_sizer.dart';
 
-import 'package:mobile/src/onboarding/presentation/pages/onboarding_screen.dart';
 
 void main() {
   runApp(const MainApp());
