a0bf01b6c3d71a71d54a5fbf30de15bf59316e02
Raphael Adjetey Adjei <100845372+RAAdjei@users.noreply.github.com>
2024-06-29 04:42:38 +0000

    feat(web): Corrections to page.tsx and ResetInputEmail.tsx

--- web/components/ResetInputEmail.tsx
+++ web/components/ResetInputEmail.tsx
@@ -15,8 +15,8 @@ const ResetInputEmail: React.FC<ResetEmailProps> = ({ onSubmit }) => {
   };
 
   return (
-    <div className="flex-col flex-nowrap w-[26rem] p-0">
-      <div className="flex justify-center items-center p-3 pl-2 pr-2 w-full text-sm font-sans">
+    <div className="flex-col w-[26rem] p-0">
+      <div className="flex justify-center items-center p-3 px-2 w-full text-sm font-sans">
         Enter your email address, you will receive a link in your email to create a new password
       </div>
 
@@ -31,14 +31,14 @@ const ResetInputEmail: React.FC<ResetEmailProps> = ({ onSubmit }) => {
                 placeholder="Email Address"
                 onChange={(e) => setEmail(e.target.value)}
                 required
-                className="font-sans rounded-3xl w-full h-12 !border !border-slate-300 border-t-transparent bg-transparent px-3 py-1.5 font-normal shadow-md outline outline-0 ring-4 ring-transparent transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:!border-gray-900 focus:border-t-transparent focus:outline-0 focus:ring-gray-500/10 disabled:border-0 disabled:bg-blue-gray-50"
+                className="font-sans rounded-3xl w-full h-12 !border !border-slate-300 border-t-transparent px-3 py-2 transition-all focus:border-1 focus:!border-gray-700"
               />
             </label>
           </div>
           <div className="pt-9 text-sm font-sans">
             <button
               type="submit"
-              className="rounded-3xl bg-terrablue w-36 h-12 text-white dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 hover:bg-blue-800 focus:ring-blue-300 font-sans"
+              className="rounded-3xl bg-terrablue w-36 h-12 text-white hover:bg-blue-800 font-sans"
             >
               Continue
             </button>
