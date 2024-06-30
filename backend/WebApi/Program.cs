7f6f2c4d6fc1a7a094f8a502ae17b1f9502eac6d
Farouk Sedick <sadikalhanssah@gmail.com>
2024-06-30 12:26:26 +0000

    Ggpp 98 implement user login (#25)
    * [fix] implement microsoft identity in dbcontext and entities
    
    * [fix] implement microsoft identity
    
    * gitignore items
    
    * gitignore migrations folder
    
    * [feature] create api result class
    
    * [feature] implement api result class
    
    * [feature] implement api result class
    
    * [feat] implement login system

--- backend/WebApi/Program.cs
+++ backend/WebApi/Program.cs
@@ -1,15 +1,27 @@
+using Application.Features.Users.LoginUser.Command;
 using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Persistence;
+using Persistence.Repositories;
+using System.Reflection;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<AppAuthDbContext>(options =>
 options.UseNpgsql(builder.Configuration.GetConnectionString("AppAuthDbConnectionString")));
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<ITokenRepository, TokenRepository>();
+
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(LoginUserCommand).Assembly));
 
 builder.Services.AddControllers();
 
@@ -23,11 +35,37 @@ builder.Services.AddApiVersioning(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    });
+
 builder.Services.AddIdentityCore<User>()
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<AppAuthDbContext>()
     .AddTokenProvider<DataProtectorTokenProvider<User>>("AppAuthDbContext")
     .AddDefaultTokenProviders();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequiredLength = 6;
+    options.Password.RequireDigit = false;
+    options.Password.RequiredUniqueChars = 1;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireDigit = false;
+
+    options.User.RequireUniqueEmail = true;
+});
 
 var app = builder.Build();
 
@@ -40,6 +78,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
