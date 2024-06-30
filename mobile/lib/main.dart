6e73e3845ed00c7168a070e7f73f913261cbd37f
elquejios <166038603+elquejios@users.noreply.github.com>
2024-06-30 15:44:20 +0000

    Ggpp 107 reset new password page (#32)
    * feat:password reset page raw structure
    
    * feat(password reset): Updated the screen inplementation to match the UI
    
    * feat:password reset page raw structure
    
    * feat(password reset): Updated the screen inplementation to match the UI
    
    * feat:password reset page raw structure
    
    * feat:password reset page raw structure
    
    * feat:password reset page raw structure
    
    * feat(password reset): Updated the screen inplementation to match the UI
    
    * fix:default error border color to original border color
    
    ---------
    
    Co-authored-by: Tariq Nasser Deen <tariqnassiru@gmail.com>

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/src/core/theme/theme_config.dart';
+import 'package:mobile/src/features/auth/presentation/password_reset.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+
 import 'package:mobile/src/features/auth/presentation/pages/otp_page.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
+
 
 void main() {
   runApp(const MainApp());
@@ -18,7 +22,7 @@ class MainApp extends StatelessWidget {
           darkTheme: ThemeConfig().darkTheme,
           theme: ThemeConfig().lightTheme,
           themeMode: ThemeMode.light,
-          home: const OTPage(),
+          home: PasswordResetScreen(),
         );
       },
     );
