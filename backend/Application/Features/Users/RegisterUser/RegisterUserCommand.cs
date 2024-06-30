7d033f0e509832a1996484811113b8f55b5ab1a6
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2024-06-30 15:21:26 +0000

    feat(backend):Add validation to RegisterUserCommand

--- backend/Application/Features/Users/RegisterUser/RegisterUserCommand.cs
+++ backend/Application/Features/Users/RegisterUser/RegisterUserCommand.cs
@@ -1,7 +1,9 @@
 ﻿using Application.Models.ApiResult;
+using FluentValidation.Validators;
 using MediatR;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +12,19 @@ namespace Application.Features.Users.RegisterUser
 {
     public class RegisterUserCommand : IRequest<Result>
     {
+        [Required, StringLength(50)]
         public string FirstName { get; set; }
+
+        [Required, StringLength(50)]
         public string LastName { get; set; }
+
+        [Required, EmailAddress]
         public string Email { get; set; }
+
+        [Required, StringLength(100, MinimumLength =6)]
         public string Password { get; set; }
+
+        [Required, StringLength(10, MinimumLength = 10, ErrorMessage = "Mobile number should be 10")]
         public string? mobileNumber { get; set; }
 
        
