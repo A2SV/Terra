82486f57530a3af18297aa73fe78e42e63984e66
Farouk Sedick <sadikalhanssah@gmail.com>
2024-06-30 12:26:26 +0000

    Ggpp 98 implement user login (#25)
    * [fix] implement microsoft identity in dbcontext and entities
    
    * [fix] implement microsoft identity
    
    * gitignore items
    
    * gitignore migrations folder
    
    * [feature] create api result class
    
    * [feature] implement api result class
    
    * [feature] implement api result class
    
    * [feat] implement login system

---
+++ backend/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly UserManager<User> userManager;
+
+        public UserRepository(UserManager<User> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public async Task<User?> GetUserByEmailAsync(string username, string password)
+        {
+            var existingUser = await userManager.FindByEmailAsync(username);
+
+            if (existingUser != null) 
+            {
+                var result = await userManager.CheckPasswordAsync(existingUser, password);
+
+                return result ? existingUser : null;
+            }
+
+            return null;
+        }
+    }
+}
