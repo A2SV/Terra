d2562f1c34eed2a24034db28ceb6f157d11aa870
Tariq Nasser Deen <tariqnassiru@gmail.com>
2024-07-02 12:58:05 +0000

    first commit

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/src/core/theme/theme_config.dart';
+import 'package:mobile/src/features/auth/presentation/pages/password_reset.dart';
 
-import 'package:mobile/src/features/auth/presentation/password_reset.dart';
+
 import 'package:responsive_sizer/responsive_sizer.dart';
 
 import 'package:mobile/src/features/auth/presentation/pages/otp_page.dart';
