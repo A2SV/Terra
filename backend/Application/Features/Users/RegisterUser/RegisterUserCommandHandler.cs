8f901dcd69798760c3daa697d3aa4e5b26cdc96f
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2024-07-03 16:10:36 +0000

    feat(backend): Implement RegisterUserCommandHandler

--- backend/Application/Features/Users/RegisterUser/RegisterUserCommandHandler.cs
+++ backend/Application/Features/Users/RegisterUser/RegisterUserCommandHandler.cs
@@ -3,6 +3,10 @@ using MediatR;
 using Application.Contracts;
 using Microsoft.AspNetCore.Identity;
 using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+
 
 namespace Application.Features.Users.RegisterUser
 {
@@ -10,11 +14,15 @@ namespace Application.Features.Users.RegisterUser
     {
         private readonly IUserRepository _userRepository;
         private readonly UserManager<User> _userManager;
-
-        public RegisterUserCommandHandler(IUserRepository userRepository, UserManager<User> userManager)
+        private readonly IOTPService _otpService;
+        private readonly IOTPRepository _otpRepository;
+        public RegisterUserCommandHandler(IUserRepository userRepository, UserManager<User> userManager,
+            IOTPService otpService, IOTPRepository otpRepository)
         {
             _userRepository = userRepository;
             _userManager = userManager;
+            _otpService = otpService;
+            _otpRepository = otpRepository;
         }
 
         public async Task<Result<User>> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
@@ -25,24 +33,38 @@ namespace Application.Features.Users.RegisterUser
                 return new Result<User>(false, ResultStatusCode.BadRequest, null, "A user with this email already exists.");
             }
 
-
             var newUser = new User
             {
                 FirstName = request.FirstName,
                 LastName = request.LastName,
                 Email = request.Email,
                 UserName = request.Email,
-                PhoneNumber = request.PhoneNumber
+                PhoneNumber = request.PhoneNumber,
+
+                
             };
 
             var result = await _userRepository.RegisterUserAsync(newUser, request.Password);
-
             if (!result.Succeeded)
             {
                 string errors = string.Join("; ", result.Errors.Select(e => e.Description));
+
                 return new Result<User>(false, ResultStatusCode.BadRequest, null, $"User creation failed: {errors}");
             }
+
+            var totp = _otpService.GenerateOTP();
+            var otp = totp.ComputeTotp();
+            var otpEntry = new OtpEntry
+            {
+                UserId = newUser.Id,
+                Otp = otp,
+                Expiry = DateTime.UtcNow.AddSeconds(30)
+            };
+            await _otpRepository.AddOtpEntryAsync(otpEntry);
+            await _otpService.SendOtpEmailAsync(newUser.Email, otp);
+            
             return new Result<User>(true, ResultStatusCode.Created, newUser, "Registration successful");
+
         }
     }
 }
