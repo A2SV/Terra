24e89166b1b508acf48fc237979909787a3b67a8
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2024-07-03 16:07:02 +0000

    feat(backend): Implement IOTPService

---
+++ backend/Application/Contracts/IOTPService.cs
@@ -0,0 +1,20 @@
+﻿using OtpNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts
+{
+    public interface  IOTPService
+    {
+        public Totp GenerateOTP();
+
+        public bool ValidateOtp(string storedOtp, string providedOtp);
+
+        public Task SendOtpEmailAsync(string email, string otp);
+        
+
+    }
+}
