4edb3ee8d32f47147ef7733ff707e0b95ccbe1dc
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2024-07-03 16:06:38 +0000

    feat(backend): Implement IOTPRepository

---
+++ backend/Application/Contracts/IOTPRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts
+{
+    public interface IOTPRepository
+    {
+        Task<OtpEntry> GetOtpEntryAsync(string userId);
+        Task<OtpEntry> AddOtpEntryAsync(OtpEntry otpEntry);
+        Task DeleteOtpEntryAsync(Guid otpId);
+    }
+}
