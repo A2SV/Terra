2ad08fa01646ec4e930947f01485ac45540f29a2
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2024-07-03 21:47:17 +0000

    feat(backend) : Add ResendOTPCommand and ResendOTPCommandHandler

---
+++ backend/Application/Features/Users/ResendOTP/ResendOTPCommandHandler.cs
@@ -0,0 +1,59 @@
+﻿using Application.Contracts;
+using Application.Models.ApiResult;
+using Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Users.ResendOTP
+{
+    public class ResendOTPCommandHandler:IRequestHandler<ResendOTPCommand, Result>
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly IOTPService _otpService;
+        private readonly IOTPRepository _otpRepository;
+
+        public ResendOTPCommandHandler(UserManager<User> userManager, IOTPService otpService, IOTPRepository otpRepository)
+        {
+            _userManager = userManager;
+            _otpService = otpService;
+            _otpRepository = otpRepository;
+        }
+
+        public async Task<Result> Handle(ResendOTPCommand request, CancellationToken cancellation)
+        {
+            var user = await _userManager.FindByEmailAsync(request.Email);
+            if (user == null)
+            {
+                return new Result(false, ResultStatusCode.NotFound, "User not found.");
+            }
+
+            var existingOtp = await _otpRepository.GetOtpEntryAsync(user.Id);
+            if (existingOtp!=null && (DateTime.UtcNow - existingOtp.Expiry).TotalMinutes<1)
+            {
+                return new Result(false, ResultStatusCode.BadRequest, "Please wait for 1 minute before requesting a new OTP.");
+            }
+
+
+            var totp = _otpService.GenerateOTP();
+            var otp = totp.ComputeTotp();
+            var otpEntry = new OtpEntry
+            {
+                UserId = user.Id,
+                Otp = otp,
+                Expiry = DateTime.UtcNow.AddSeconds(60),
+                
+            };
+
+            await _otpRepository.UpdateOtpEntryAsync(otpEntry);
+            await _otpService.SendOtpEmailAsync(user.Email, otp);
+
+            return new Result(true, ResultStatusCode.Success, "OTP has been resent successfully.");
+
+        }
+    }
+}
