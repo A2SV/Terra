ef146e552e5f9fe42b67e59a459cbf02d9c49623
Ohene-Adjei George Kofi <georgeey123@gmail.com>
2024-07-03 21:48:26 +0000

    Ggpp 102 implement sign in with google (#47)
    * feat(backend): add extra authentication schemes for google sign in
    
    * feat(backend): Implement Google login functionality in AuthController
    
    * fix(backend): move solution file
    
    * feat(backed): add redirect url to google sign-in
    
    * fix(backend): fix auth scheme bug
    
    * fix(backend): increase password minimum length

--- backend/WebApi/Controllers/AuthController.cs
+++ backend/WebApi/Controllers/AuthController.cs
@@ -5,7 +5,15 @@ using Application.Features.Users.RegisterUser;
 using Application.Features.Users.ResetPassword;
 using Application.Models.ApiResult;
 using MediatR;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Domain.Entities;
+using Application.Contracts;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 
 
 namespace WebApi.Controllers
@@ -15,10 +23,14 @@ namespace WebApi.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IMediator mediator;
+        private readonly UserManager<User> userManager;
+        private readonly ITokenRepository tokenRepository;
 
-        public AuthController(IMediator mediator)
+        public AuthController(IMediator mediator, UserManager<User> userManager, ITokenRepository tokenRepository)
         {
             this.mediator = mediator;
+            this.userManager = userManager;
+            this.tokenRepository = tokenRepository;
         }
 
         [HttpPost("login")]
@@ -46,7 +58,7 @@ namespace WebApi.Controllers
 
             return Created($"/User/{result.Data.Id}", "User registration successful");
         }
-
+    
         [HttpPost("reset-password")]
         public async Task<IActionResult> ResetPassword([FromBody] ResetPasswordDto resetPasswordDto)
         {
@@ -98,6 +110,67 @@ namespace WebApi.Controllers
                 var result = new Result(false, ResultStatusCode.BadRequest, "An error occured while sending forgot password email.");
                 return StatusCode((int)result.StatusCode, result);
             }
+        }
+
+        [HttpGet("google-login")]
+        [AllowAnonymous]
+        public IActionResult GoogleLogin()
+        {
+            // var properties = new AuthenticationProperties { RedirectUri = "http://localhost:5183/api/Auth/google-response" };
+            var properties = new AuthenticationProperties
+            {
+                RedirectUri = Url.Action("GoogleResponse", "Auth", null, Request.Scheme),
+                Items =
+                {
+                    {"scheme", GoogleDefaults.AuthenticationScheme}
+                }
+            };
+            Console.WriteLine($"RedirectUri: {properties.RedirectUri}");
+            return Challenge(properties, GoogleDefaults.AuthenticationScheme);
+        }
+
+        [HttpGet("google-response")]
+        public async Task<IActionResult> GoogleResponse()
+        {
+            var authenticateResult = await HttpContext.AuthenticateAsync(GoogleDefaults.AuthenticationScheme);
+            if (!authenticateResult.Succeeded)
+            {
+                return BadRequest("Google authentication failed");
+            }
+
+            var claims = authenticateResult.Principal?.Identities.FirstOrDefault()?.Claims;
+            var emailClaim = claims?.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
+            // var email = emailClaim?.Value;
+
+            if (String.IsNullOrEmpty(emailClaim))
+            {
+                return BadRequest("There is no user with this email");
+            }
+
+            var user = await userManager.FindByEmailAsync(emailClaim);
+
+            if (user == null)
+            {
+                user = new User
+                {
+                    Email = emailClaim,
+                    UserName = emailClaim,
+                    FirstName = claims.FirstOrDefault(c => c.Type == ClaimTypes.GivenName)?.Value,
+                    LastName = claims.FirstOrDefault(c => c.Type == ClaimTypes.Surname)?.Value,
+                    EmailConfirmed = true
+                };
+
+                var result = await userManager.CreateAsync(user);
+                if (!result.Succeeded)
+                {
+                    return BadRequest("User creation failed");
+                }
+            }
+
+            var userRoles = await userManager.GetRolesAsync(user);
+            var jwtToken = tokenRepository.GenerateJwtToken(user, userRoles.ToArray());
+
+            return Ok(new { Token = jwtToken });
         }
     }
 }
