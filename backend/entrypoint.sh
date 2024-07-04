1a49996156877f719c7e8ef62393f6331dd78f1c
Tibebu <tibebugwas@gmail.com>
2024-07-04 22:40:49 +0000

    feat(backend): create the dockerfile

---
+++ backend/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e  # Stop on error
+
+echo "Running EF migrations..."
+# Uncomment the next line if migrations need to be applied at runtime
+dotnet ef database update --project ./Persistence/Persistence.csproj
+
+echo "Starting application..."
+dotnet ./WebApi.dll
