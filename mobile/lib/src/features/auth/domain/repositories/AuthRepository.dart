bb40af80ce35d4c256705d6f5c7c35ec4c01c5ad
elquejios <166038603+elquejios@users.noreply.github.com>
2024-07-04 22:25:34 +0000

    feat(forgot password): Implement the forgot password page (#58)
    Co-authored-by: Tariq Nasser Deen <tariqnassiru@gmail.com>

--- mobile/lib/src/features/auth/domain/repositories/AuthRepository.dart
+++
@@ -1,8 +0,0 @@
-
-
-import 'package:dartz/dartz.dart';
-import 'package:mobile/src/features/auth/domain/entities/entities.dart';
-
-abstract class AuthRepository{
-  Future<Either<String,User>> userLogin(String username, String password);
-}
