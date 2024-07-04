e9d35b5b1a4420d674c579f930f1da416c4bdde7
elquejios <166038603+elquejios@users.noreply.github.com>
2024-07-04 22:25:34 +0000

    feat(forgot password): Implement the forgot password page (#58)
    Co-authored-by: Tariq Nasser Deen <tariqnassiru@gmail.com>

--- mobile/lib/src/features/auth/presentation/pages/forgot_password.dart
+++ mobile/lib/src/features/auth/presentation/pages/forgot_password.dart
@@ -3,16 +3,14 @@ import 'package:mobile/src/core/theme/common_color.dart';
 import 'package:mobile/src/core/theme/text_theme.dart';
 import 'package:mobile/src/core/utils/custom_textformfield.dart';
 import 'package:mobile/src/core/widgets/custom_button.dart';
-import 'package:mobile/src/features/onboarding/presentation/widgets/page_indicator.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
-
 import '../../../onboarding/presentation/widgets/page_indicator.dart';
 
-class PasswordResetScreen extends StatefulWidget {
-  const PasswordResetScreen({super.key});
+class ForgotPasswordScreen extends StatefulWidget {
+  const ForgotPasswordScreen({super.key});
 
   @override
-  State<PasswordResetScreen> createState() => _PasswordResetScreenState();
+  State<ForgotPasswordScreen> createState() => _ForgotPasswordScreenState();
 }
 
 TextEditingController passwordController = TextEditingController();
@@ -21,22 +19,30 @@ bool passwordVisibility = true;
 TextEditingController passwordController1 = TextEditingController();
 bool passwordVisibility1 = true;
 
-class _PasswordResetScreenState extends State<PasswordResetScreen> {
+class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> with SingleTickerProviderStateMixin {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        resizeToAvoidBottomInset: true,
-        backgroundColor: Theme.of(context).colorScheme.onPrimary,
-        appBar: AppBar(
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back),
-            onPressed: () {},
-          ),
-        ),
-        body: SingleChildScrollView(
+      resizeToAvoidBottomInset: true,
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              Row(
+                children: [
+                  IconButton(
+                    icon: Icon(
+                      Icons.arrow_back,
+                      size: 7.w,
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+              SizedBox(height: 2.h),
               Container(
                   alignment: Alignment.topCenter,
                   key: const Key('page-indicator'),
@@ -47,7 +53,8 @@ class _PasswordResetScreenState extends State<PasswordResetScreen> {
                         margin: EdgeInsets.fromLTRB(2.w, 0, 2.w, 0),
                         child: PageIndicator(
                           width: 10.w,
-                          color: AppCommonColors.mainlightBlue,
+                          color: AppCommonColors.mainBlueButton
+                          ,
                         ),
                       ),
                       Container(
@@ -61,7 +68,7 @@ class _PasswordResetScreenState extends State<PasswordResetScreen> {
                         margin: EdgeInsets.fromLTRB(2.w, 0, 2.w, 0),
                         child: PageIndicator(
                           width: 10.w,
-                          color: AppCommonColors.mainBlueButton,
+                          color: AppCommonColors.mainlightBlue,
                         ),
                       ),
                     ],
@@ -72,48 +79,56 @@ class _PasswordResetScreenState extends State<PasswordResetScreen> {
               Container(
                 key: const Key('image'),
                 margin: EdgeInsets.all(5.h),
+                padding: EdgeInsets.all(1.h),
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(20),
                   color: AppCommonColors.imageBackgroundColor.withOpacity(0.75),
                 ),
                 child: Image.asset(
-                  'assets/images/lock.png',
-                  width: 25.w,
-                  height: 25.w,
+                  'assets/images/mail.png',
+                  width: 23.w,
+                  height: 23.w,
+
                 ),
               ),
               Container(
-                key: const Key('Reset Password'),
+                key: const Key('forgot password'),
                 margin: EdgeInsets.all(1.h),
                 child: Text(
-                  'Reset Password',
+                  'Forgot Password?',
                   style: CustomTextStyles.kDefaultTextTheme(
-                          AppCommonColors.defaultLink)
+                      AppCommonColors.defaultLink)
                       .displayMedium,
                 ),
               ),
               Container(
-                key: const Key('Description'),
-                margin: EdgeInsets.all(1.h),
-                child: Text(
-                  'Reset your password and join the terra family today',
-                  style: CustomTextStyles.kDefaultTextTheme(
+                  key: const Key('Description'),
+                  margin: EdgeInsets.all(1.h),
+                  alignment: Alignment.center,
+                  width: 85.w,
+
+                  child: Center(
+                    child:Text(
+                      'Forgot your password?  Reset it now and get back to the Terra family!',
+                      style: CustomTextStyles.kDefaultTextTheme(
                           AppCommonColors.signInWithGoogleBgnd)
-                      .bodyMedium,
-                ),
+                          .bodyMedium,
+                      textAlign: TextAlign.center,
+                    ),
+                  )
               ),
               SizedBox(
                 height: 2.h,
               ),
               Container(
-                key: const Key('New Password'),
+                key: const Key('Email'),
                 child: SizedBox(
                     width: 85.w,
                     height: 10.h,
                     child: CustomTextFormField(
-                      textFormFieldType: TextFormFieldType.password,
+                      textFormFieldType: TextFormFieldType.regular,
                       controller: passwordController,
-                      hintText: 'New password',
+                      hintText: 'Email',
                       borderSideColor: AppCommonColors.textFieldTextColor,
                       prefixIcon: SizedBox(
                         width: 15.w,
@@ -123,50 +138,18 @@ class _PasswordResetScreenState extends State<PasswordResetScreen> {
                               width: 5.w,
                             ),
                             const Icon(
-                              Icons.lock_outline,
+                              Icons.email_outlined,
                               color: AppCommonColors.textFieldTextColor,
                             ),
                           ],
                         ),
                       ),
                       hintStyle: CustomTextStyles.kDefaultTextTheme(
-                              AppCommonColors.textFieldTextColor)
+                          AppCommonColors.textFieldTextColor)
                           .bodyMedium,
                       contentPadding: EdgeInsets.symmetric(vertical: 5.w),
                       style: CustomTextStyles.kDefaultTextTheme(
-                              AppCommonColors.textFieldTextColor)
-                          .bodyMedium,
-                    )),
-              ),
-              Container(
-                key: const Key('Confirm new password'),
-                child: SizedBox(
-                    width: 85.w,
-                    height: 10.h,
-                    child: CustomTextFormField(
-                      textFormFieldType: TextFormFieldType.password,
-                      borderSideColor: AppCommonColors.textFieldTextColor,
-                      hintText: 'Confirm New password',
-                      prefixIcon: SizedBox(
-                        width: 15.w,
-                        child: Row(
-                          children: [
-                            SizedBox(
-                              width: 5.w,
-                            ),
-                            const Icon(
-                              Icons.lock_outline,
-                              color: AppCommonColors.textFieldTextColor,
-                            ),
-                          ],
-                        ),
-                      ),
-                      hintStyle: CustomTextStyles.kDefaultTextTheme(
-                              AppCommonColors.textFieldTextColor)
-                          .bodyMedium,
-                      contentPadding: EdgeInsets.symmetric(vertical: 5.w),
-                      style: CustomTextStyles.kDefaultTextTheme(
-                              AppCommonColors.textFieldTextColor)
+                          AppCommonColors.textFieldTextColor)
                           .bodyMedium,
                     )),
               ),
@@ -174,9 +157,9 @@ class _PasswordResetScreenState extends State<PasswordResetScreen> {
                 key: const Key('button'),
                 width: 100.w,
                 child: CustomButton(
+                  onPressed: (){},
                   backgroundColor: AppCommonColors.mainBlueButton,
-                  text: 'Reset Password',
-                  onPressed: () {},
+                  text: 'Continue',
                   borderColor: AppCommonColors.mainBlueButton,
                   width: 85.w,
                 ),
@@ -186,6 +169,8 @@ class _PasswordResetScreenState extends State<PasswordResetScreen> {
               ),
             ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
