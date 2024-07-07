86594d47a4747515a134a493251a0c19fcca7f71
Tibebu <tibebugwas@gmail.com>
2024-07-07 20:42:48 +0000

    feat(backend): revert changes on entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,9 +2,8 @@
 set -e  # Stop on error
 
 echo "Running EF migrations..."
-dotnet tool install --global dotnet-ef
 # Uncomment the next line if migrations need to be applied at runtime
-dotnet ef database update --project ./Persistence/Persistence.csproj
+# dotnet ef database update --project ./Persistence/Persistence.csproj
 
 echo "Starting application..."
 dotnet ./WebApi.dll
