2db5fea2bd6e85d0e2b5bda792d550e82d63f865
Tibebu <tibebugwas@gmail.com>
2024-07-08 17:44:06 +0000

    feat(backend): update persistence snapshot

--- backend/Persistence/Configurations/AppAuthDbContextFactory.cs
+++ backend/Persistence/Configurations/AppAuthDbContextFactory.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 using Persistence.Configurations;
+using DotNetEnv;
 
 public class AppAuthDbContextFactory : IDesignTimeDbContextFactory<AppAuthDbContext>
 {
@@ -14,7 +15,14 @@ public class AppAuthDbContextFactory : IDesignTimeDbContextFactory<AppAuthDbCont
             .Build();
 
         var builder = new DbContextOptionsBuilder<AppAuthDbContext>();
-        var connectionString = configuration.GetConnectionString("AppAuthDbConnectionString");
+        DotNetEnv.Env.Load("../.env");
+        var host = System.Environment.GetEnvironmentVariable("DB_HOST");
+        var user = System.Environment.GetEnvironmentVariable("DB_USER");
+        var password = System.Environment.GetEnvironmentVariable("DB_PASS");
+        var database = System.Environment.GetEnvironmentVariable("DB_NAME");
+        var port = System.Environment.GetEnvironmentVariable("DB_PORT");
+        var pooling = System.Environment.GetEnvironmentVariable("DB_POOLING");
+        var connectionString = $"Host={host}; Database={database};Username={user};Password={password};";
         builder.UseNpgsql(connectionString);
 
         return new AppAuthDbContext(builder.Options);
--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,7 +3,7 @@ set -e  # Stop on error
 
 echo "Running EF migrations..."
 # Uncomment the next line if migrations need to be applied at runtime
-# dotnet ef database update --project ./Persistence/Persistence.csproj
+dotnet ef database update --project ./Persistence/Persistence.csproj
 
 echo "Starting application..."
 dotnet ./WebApi.dll
