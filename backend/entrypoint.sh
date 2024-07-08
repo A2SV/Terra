3f4bfccad03ad96e7671aeb8b2ff39cf9385be57
Tibebu <tibebugwas@gmail.com>
2024-07-08 18:15:12 +0000

    feat(backend): sdk image in final build

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,7 +2,6 @@
 set -e  # Stop on error
 
 echo "Running EF migrations..."
-# Uncomment the next line if migrations need to be applied at runtime
 dotnet ef database update --project ./Persistence/Persistence.csproj
 
 echo "Starting application..."
