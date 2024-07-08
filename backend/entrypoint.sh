d96fd12d20f5e2f3628a8ebd817a10069e066d04
George <georgeey123@gmail.com>
2024-07-08 20:58:26 +0000

    Fix(backend): remove migrations from entry point

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e  # Stop on error
 
 echo "Running EF migrations..."
-dotnet ef database update --project ./Persistence/Persistence.csproj
+#dotnet ef database update --project ./Persistence/Persistence.csproj
 
 echo "Starting application..."
 dotnet WebApi.dll
