b1740337887f81aa41319fba68cf133608b10aab
George <georgeey123@gmail.com>
2024-07-09 18:51:00 +0000

    fix(backend): include the directory where dotnet-ef is installed and export path

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e  # Stop on error
 
+# Ensure the PATH includes the directory where dotnet-ef is installed
+export PATH="$PATH:/root/.dotnet/tools"
+
 echo "Running EF migrations..."
 dotnet ef database update --project /app/Persistence/Persistence.csproj
 
