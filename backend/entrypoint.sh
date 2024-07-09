3d5bb8d9acd8682d501b940a0dd50c7e88932f37
George <georgeey123@gmail.com>
2024-07-09 18:12:11 +0000

    fix(backend): add dotnet 2.1 sdk and migration update to docker file

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e  # Stop on error
 
 echo "Running EF migrations..."
-dotnet dotnet-ef database update --project /app/Persistence/Persistence.csproj
+#dotnet dotnet-ef database update --project /app/Persistence/Persistence.csproj
 
 echo "Starting application..."
 dotnet WebApi.dll
