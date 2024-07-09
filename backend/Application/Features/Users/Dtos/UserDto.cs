af79aac7353e0396584633910fcd82f52d7db824
sedfarouk <sadikalhanssah@gmail.com>
2024-07-09 16:30:46 +0000

    fix: add id to returned getuserbyid object

--- backend/Application/Features/Users/Dtos/UserDto.cs
+++ backend/Application/Features/Users/Dtos/UserDto.cs
@@ -8,6 +8,7 @@ namespace Application.Features.Users.Dtos
 {
     public class UserDto
     {
+        public Guid Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
