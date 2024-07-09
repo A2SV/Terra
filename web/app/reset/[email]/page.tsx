7728f7d6b9e9d485cf80f2076ba8e8d5b57d3cd1
lit <103055119+EricaAnnor@users.noreply.github.com>
2024-07-09 21:11:43 +0000

    feat(web): add otp verification functionality

---
+++ web/app/reset/[email]/page.tsx
@@ -0,0 +1,12 @@
+import ResetNew from "@/components/ResetNew";
+import React from "react";
+
+const ResetPasswordPage = () => {
+  return (
+    <div>
+      <ResetNew />
+    </div>
+  );
+};
+
+export default ResetPasswordPage;
