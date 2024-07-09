d5f94d0c681f517975dc3a56f05fe0a983f2d16d
sedfarouk <sadikalhanssah@gmail.com>
2024-07-09 13:39:57 +0000

    feat: return user details after login

---
+++ backend/Application/Features/Users/LoginUser/Command/LoginUserCommandHandler.cs
@@ -0,0 +1,56 @@
+using Application.Contracts;
+using Application.Models.ApiResult;
+using Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.Features.Users.LoginUser.Command
+{
+    public class LoginUserCommandHandler : IRequestHandler<LoginUserCommand, TokenUserResult>
+    {
+        private readonly UserManager<User> userManager;
+        private readonly IUserRepository userRepository;
+        private readonly ITokenRepository tokenRepository;
+
+        public LoginUserCommandHandler(UserManager<User> userManager,IUserRepository userRepository, ITokenRepository tokenRepository)
+        {
+            this.userManager = userManager;
+            this.userRepository = userRepository;
+            this.tokenRepository = tokenRepository;
+        }
+
+        public async Task<TokenUserResult> Handle(LoginUserCommand request, CancellationToken cancellationToken)
+        {
+            var existingUser = await userRepository.GetUserByEmailAsync(request.UserName, request.Password);
+
+            if (existingUser == null)
+            {
+                return new TokenUserResult(
+                    false,
+                    ResultStatusCode.BadRequest,
+                    "User login failed",
+                    string.Empty,
+                    new {}
+                );
+            }
+
+            var userRoles = await userManager.GetRolesAsync(existingUser);
+
+            var JwtToken = tokenRepository.GenerateJwtToken(existingUser, userRoles.ToArray());
+
+            return new TokenUserResult(
+                    true,
+                    ResultStatusCode.Created,
+                    "User login successful",
+                    JwtToken,
+                    new
+                    {
+                        FirstName = existingUser.FirstName,
+                        LastName = existingUser.LastName,
+                        Email = existingUser.Email,
+                        ProfilePicture = existingUser.ProfilePictureUrl
+                    }
+                );
+        }
+    }
+}
