4d97fa276999b780dfb190f79fc7641d3208ad7e
George <georgeey123@gmail.com>
2024-07-10 14:32:48 +0000

    fix(backend): add inf for debug

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,6 +5,7 @@ set -e  # Stop on error
 export PATH="$PATH:/root/.dotnet/tools"
 
 echo "Running EF migrations..."
+dotnet dotnet --info
 dotnet dotnet-ef database update --project /app/Persistence/Persistence.csproj
 
 echo "Starting application..."
