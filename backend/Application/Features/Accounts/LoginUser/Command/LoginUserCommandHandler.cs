2cf8b7da7265f50487475dc564514375f8e1bd42
sedfarouk <sadikalhanssah@gmail.com>
2024-07-12 16:34:07 +0000

    stfu

--- backend/Application/Features/Accounts/LoginUser/Command/LoginUserCommandHandler.cs
+++ backend/Application/Features/Accounts/LoginUser/Command/LoginUserCommandHandler.cs
@@ -21,11 +21,7 @@ namespace Application.Features.Accounts.LoginUser.Command
 
         public async Task<TokenUserResult> Handle(LoginUserCommand request, CancellationToken cancellationToken)
         {
-<<<<<<< HEAD
             var existingUser = await userRepository.GetUserByEmailAsync(request.Email, request.Password);
-=======
-            var existingUser = await userRepository.GetUserByEmailAsync(request.UserName, request.Password);
->>>>>>> 25ac370 (feat: create endpoint to get user by id)
 
             if (existingUser == null)
             {
@@ -49,10 +45,7 @@ namespace Application.Features.Accounts.LoginUser.Command
                     JwtToken,
                     new
                     {
-<<<<<<< HEAD
                         Id = existingUser.Id,
-=======
->>>>>>> 25ac370 (feat: create endpoint to get user by id)
                         FirstName = existingUser.FirstName,
                         LastName = existingUser.LastName,
                         Email = existingUser.Email,
