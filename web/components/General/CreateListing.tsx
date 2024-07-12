5ca522ccc74238cab3af14cc05234b1693070355
lit <103055119+EricaAnnor@users.noreply.github.com>
2024-07-12 22:08:05 +0000

    resolved conversation

--- web/components/General/CreateListing.tsx
+++ web/components/General/CreateListing.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const CreateListing = () => {
   return (
-    <div className="bg-[#F5F5F5E8]">
+    <div className="bg-backgroundColor">
       <div className="w-full  flex lg:flex-row flex-col justify-between items-center  md:pl-12 md:pr-12 space-y-10 lg:pt-[30px] lg:pb-[30px] md:pb-20">
         <div className=" flex flex-row relative  items-center justify-center  md:w-1/2 w-full ">
           <div className="z-20  relative ">
@@ -15,36 +15,36 @@ const CreateListing = () => {
         <div className=" md:w-1/2 w-full  flex flex-row items-center justify-center  px-auto">
           <div className="flex flex-col space-y-6 relative z-20">
             <div className=" md:w-[400px]  w-[380px]">
-              <h1 className="text-[28px] font-bold text-[#3A3A3A]  font-nunito">
+              <h1 className="text-[28px] font-bold text-normalTextColor  font-nunito">
                 Got any property you want to sell or lease?
               </h1>
             </div>
 
             <div className=" md:w-[400px] w-[380px]">
-              <h1 className="text-[18px]  text-[#3A3A3A] font-bold  font-nunito">
+              <h1 className="text-[18px]  text-normalTextColor font-bold  font-nunito">
                 Click the button below to create listings of your property
               </h1>
             </div>
 
             <div className="flex justify-start">
               <ul className="list-none pl-3 space-y-5  ">
-                <li className="relative text-[18px] text-[#3A3A3A] pl-2  font-nunito">
-                  <span className="absolute left-0 top-0 inline-block w-[6px] h-[6px] rounded-full bg-[#05E200] transform -translate-x-3 translate-y-2 "></span>
+                <li className="relative text-[18px] text-normalTextColor pl-2  font-nunito">
+                  <span className="absolute left-0 top-0 inline-block w-[6px] h-[6px] rounded-full bg-bulletColor transform -translate-x-3 translate-y-2 "></span>
                   Easy and Efficient Listing Process
                 </li>
-                <li className="relative text-[18px] text-[#3A3A3A] pl-2  font-nunito">
-                  <span className="absolute left-0 top-0 inline-block w-[6px] h-[6px] rounded-full bg-[#05E200] transform -translate-x-3 translate-y-2"></span>
+                <li className="relative text-[18px] text-normalTextColor pl-2  font-nunito">
+                  <span className="absolute left-0 top-0 inline-block w-[6px] h-[6px] rounded-full bg-bulletColor transform -translate-x-3 translate-y-2"></span>
                   Reach a Wide Audience
                 </li>
-                <li className="relative text-[18px] text-[#3A3A3A] pl-2  font-nunito">
-                  <span className="absolute left-0 top-0 inline-block w-[6px] h-[6px] rounded-full bg-[#05E200] transform -translate-x-3 translate-y-2"></span>
+                <li className="relative text-[18px] text-normalTextColor pl-2  font-nunito">
+                  <span className="absolute left-0 top-0 inline-block w-[6px] h-[6px] rounded-full bg-bulletColor transform -translate-x-3 translate-y-2"></span>
                   Highlight Your Property’s Best Features
                 </li>
               </ul>
             </div>
 
             <div className="w-[320px]">
-              <button className="bg-[#1779F3] text-white font-bold text-[16px] px-6 py-3 rounded-[25px]  font-nunito">
+              <button className="bg-terrablue text-white font-bold text-[16px] px-6 py-3 rounded-[25px]  font-nunito">
                 Create Listing
               </button>
             </div>
