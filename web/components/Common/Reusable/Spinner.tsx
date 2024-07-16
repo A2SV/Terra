3b695b97bc33c4e1157af9abd83461958d57df6f
Kassimmm <tawfickkassimmohammed@gmail.com>
2024-07-16 00:13:32 +0000

    feat(web): reimplement spinner

--- web/components/Common/Reusable/Spinner.tsx
+++ web/components/Common/Reusable/Spinner.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const SpinnerComponent = () => {
+const Spinner = () => {
   return (
     <div className="border-2 border-t-4 border-t-transparent border-white rounded-full animate-spin size-5 mx-auto"></div>
   );
 };
 
-export default SpinnerComponent;
+export default Spinner;
