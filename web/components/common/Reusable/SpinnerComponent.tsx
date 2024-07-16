a445a1ba5c62f62b3358d19ba64bb8632a23f7e0
Kassimmm <tawfickkassimmohammed@gmail.com>
2024-07-16 00:27:17 +0000

    feat(web): deleted spinner component

--- web/components/common/Reusable/SpinnerComponent.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const SpinnerComponent = () => {
-  return (
-    <div className="border-2 border-t-4 border-t-transparent border-white rounded-full animate-spin size-5 mx-auto"></div>
-  );
-};
-
-export default SpinnerComponent;
