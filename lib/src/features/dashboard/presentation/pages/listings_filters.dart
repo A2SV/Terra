02e1dda92395e960126362d3ce7e325b9d69ae95
FelixNotter <shepherdeinstein@gmail.com>
2024-07-17 07:49:33 +0000

    fix(mobile): fixed rebase issues

---
+++ lib/src/features/dashboard/presentation/pages/listings_filters.dart
@@ -0,0 +1,380 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:intl/intl.dart';
+import 'package:mobile/src/core/theme/app_light_theme_colors.dart';
+import 'package:mobile/src/core/utils/utils.dart';
+import 'package:mobile/src/features/dashboard/presentation/widgets/dropdown_component.dart';
+import 'package:mobile/src/features/dashboard/presentation/widgets/filter_button.dart';
+import 'package:mobile/src/features/dashboard/presentation/widgets/search_input.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+import 'package:syncfusion_flutter_sliders/sliders.dart';
+
+class ListingsFilters extends StatefulWidget {
+  const ListingsFilters({super.key});
+
+  @override
+  State<ListingsFilters> createState() => _ListingsFiltersState();
+
+}
+
+class _ListingsFiltersState extends State<ListingsFilters> {
+  SfRangeValues _values = const SfRangeValues(0.3, 0.7);
+  @override
+  Widget build(BuildContext context) {
+    var values = const RangeValues(20, 60);
+    return Scaffold(
+            backgroundColor: AppLightThemeColors.kFieldBackgroundColor,
+            body: Padding(
+              padding: EdgeInsets.all(4.5.w),
+              child: ListView(
+                children: [
+                  // Search Bar
+                  TopBarSearchInput(
+                    key: Key('search-input'),
+                    sideContainerColor: AppLightThemeColors.kMainBlueButton,
+                    sideCotainerTextColor: AppLightThemeColors.kMainLightButton,
+                  ),
+                  //Property type Drop Down
+                  DropdownComponent(
+                    title: 'Property Type',
+                    tiles: [
+                      FilterButton(
+                        text: 'Any',
+                        onSelect: () {},
+                        isSelected: true,
+                      ),
+                      FilterButton(
+                        text: 'Any',
+                        onSelect: () {},
+                        isSelected: false,
+                      ),
+                      FilterButton(
+                        text: 'Shops',
+                        onSelect: () {},
+                        isSelected: true,
+                      ),
+                      FilterButton(
+                        text: 'Offices',
+                        onSelect: () {},
+                        isSelected: false,
+                      ),
+                      FilterButton(
+                        text: 'Shared',
+                        onSelect: () {},
+                        isSelected: false,
+                      ),
+                      FilterButton(
+                        text: 'Apartments',
+                        onSelect: () {},
+                        isSelected: true,
+                      ),
+                      FilterButton(
+                        text: 'Student Hostels',
+                        onSelect: () {},
+                        isSelected: false,
+                      ),
+                      FilterButton(
+                        text: 'Commercial',
+                        onSelect: () {},
+                        isSelected: false,
+                      ),
+                      FilterButton(
+                        text: 'Warehouses',
+                        onSelect: () {},
+                        isSelected: false,
+                      ),
+                      FilterButton(
+                        text: 'Residential',
+                        onSelect: () {},
+                        isSelected: false,
+                      ),
+                    ],
+                  ),
+
+                  //Price Range
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 18.0),
+                    child: Text("Price Range"),
+                  ),
+                  SfRangeSlider(
+                    min: 100,
+                    max: 3000,
+                    interval: 500,
+                    activeColor: AppLightThemeColors.kMainBlueButton,
+                    stepSize: 5,
+                    values: _values,
+                    showLabels: true,
+                    showTicks: true,
+                    numberFormat: NumberFormat("\$"),
+                    onChanged: (value) {
+                      setState(() {
+                        _values = value;
+                      });
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  //listview of payment methods
+                  SizedBox(
+                    height: 6.h,
+                    child: ListView.builder(
+                      itemCount: 5,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        return FilterButton(
+                          text: 'Any',
+                          onSelect: () {},
+                          isSelected: true,
+                        );
+                      },
+                    ),
+                  ),
+                  //Rooms and beds
+                  DropdownComponent(title: 'Rooms and Beds', trail: "see more",tiles: [
+                    Padding(
+                      padding: EdgeInsets.all(12.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Text('Bathroom'),
+                              SizedBox(
+                                width: 12,
+                              ),
+                              SvgPicture.asset(
+                                      'assets/svg/iconoir_bathroom.svg')
+                                  .rightPadding(16.0),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              SvgPicture.asset('assets/svg/add-circle.svg')
+                                  .rightPadding(16.0),
+                              Text('0'),
+                              SvgPicture.asset('assets/svg/minus-cirlce.svg')
+                                  .leftPadding(16.0),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(12.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Text('Bedroom'),
+                              SizedBox(
+                                width: 12,
+                              ),
+                              SvgPicture.asset(
+                                      'assets/svg/iconoir_bathroom.svg')
+                                  .rightPadding(16.0),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              SvgPicture.asset('assets/svg/add-circle.svg')
+                                  .rightPadding(16.0),
+                              Text('0'),
+                              SvgPicture.asset('assets/svg/minus-cirlce.svg')
+                                  .leftPadding(16.0),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ]),
+                  DropdownComponent(
+                    title: 'Amenities',
+                    tiles: [
+                      FilterButton(
+                        text: 'Any',
+                        onSelect: () {},
+                        isSelected: false,
+                      ),
+                      FilterButton(
+                        text: 'WiFi',
+                        onSelect: () {},
+                        isSelected: true,
+                      ),
+                      FilterButton(
+                        text: 'Balcony',
+                        onSelect: () {},
+                        isSelected: false,
+                      ),
+                      FilterButton(
+                        text: 'Security',
+                        onSelect: () {},
+                        isSelected: true,
+                      ),
+                      FilterButton(
+                        text: 'Free Parking',
+                        onSelect: () {},
+                        isSelected: false,
+                      ),
+                      FilterButton(
+                        text: 'Kitchen',
+                        onSelect: () {},
+                        isSelected: false,
+                      ),
+                      FilterButton(
+                        text: 'Air Conditioner',
+                        onSelect: () {},
+                        isSelected: false,
+                      ),
+                    ],
+                  ),
+                  DropdownComponent(
+                    title: 'Furnishing',
+                    tiles: [
+                      FilterButton(
+                        text: 'Any',
+                        onSelect: () {},
+                        isSelected: true,
+                      ),
+                      FilterButton(
+                        text: 'Full',
+                        onSelect: () {},
+                        isSelected: false,
+                      ),
+                      FilterButton(
+                        text: 'Partial',
+                        onSelect: () {},
+                        isSelected: false,
+                      ),
+                      FilterButton(
+                        text: 'None',
+                        onSelect: () {},
+                        isSelected: true,
+                      ),
+                    ],
+                  ),
+                  DropdownComponent(
+                    title: 'Lease Duration',
+                    tiles: [
+                      FilterButton(
+                        text: 'Any',
+                        onSelect: () {},
+                        isSelected: true,
+                      ),
+                      FilterButton(
+                        text: 'Full',
+                        onSelect: () {},
+                        isSelected: false,
+                      ),
+                      FilterButton(
+                        text: 'Partial',
+                        onSelect: () {},
+                        isSelected: true,
+                      ),
+                      FilterButton(
+                        text: 'None',
+                        onSelect: () {},
+                        isSelected: false,
+                      ),
+                    ],
+                  ),
+                  DropdownComponent(
+                    title: 'Pet Policy',
+                    tiles: [
+                      FilterButton(
+                        text: 'Any',
+                        onSelect: () {},
+                        isSelected: false,
+                      ),
+                      FilterButton(
+                        text: 'Pets Allowed',
+                        onSelect: () {},
+                        isSelected: true,
+                      ),
+                      FilterButton(
+                        text: 'No Pets Allowed',
+                        onSelect: () {},
+                        isSelected: false,
+                      ),
+                    ],
+                  ),
+                  DropdownComponent(
+                    title: 'Accessibility Features',
+                    tiles: [
+                      FilterButton(
+                        text: 'Any',
+                        onSelect: () {},
+                        isSelected: true,
+                      ),
+                      FilterButton(
+                        text: 'Wheelchair ',
+                        onSelect: () {},
+                        isSelected: false,
+                      ),
+                      FilterButton(
+                        text: 'Ground Floor',
+                        onSelect: () {},
+                        isSelected: false,
+                      ),
+                    ],
+                  ),
+                  DropdownComponent(
+                    title: 'Student Accommodation',
+                    tiles: [
+                      FilterButton(
+                        text: 'Off-campus',
+                        onSelect: () {},
+                        isSelected: false,
+                      ),
+                      FilterButton(
+                        text: 'On-campus ',
+                        onSelect: () {},
+                        isSelected: true,
+                      ),
+                      FilterButton(
+                        text: 'Single rooms',
+                        onSelect: () {},
+                        isSelected: false,
+                      ),
+                      FilterButton(
+                        text: 'Shared rooms',
+                        onSelect: () {},
+                        isSelected: false,
+                      ),
+                      FilterButton(
+                        text: 'Male rooms',
+                        onSelect: () {},
+                        isSelected: true,
+                      ),
+                      FilterButton(
+                        text: 'Female rooms',
+                        onSelect: () {},
+                        isSelected: false,
+                      ),
+                      FilterButton(
+                        text: 'Co-ed',
+                        onSelect: () {},
+                        isSelected: false,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      FilterButton(
+                   
+                          text: "Reset All",
+                          onSelect: () {},
+                          isSelected: false),
+                      FilterButton(
+                          text: "Show Results",
+                          onSelect: () {},
+                          isSelected: true),
+                    ],
+                  )
+                  //
+                ],
+              ),
+            ),
+          );
+  }
+}
--- lib/src/features/dashboard/presentation/widgets/filter_button.dart
+++ lib/src/features/dashboard/presentation/widgets/filter_button.dart
@@ -12,47 +12,51 @@ class FilterButton extends StatelessWidget {
   });
 
   final String text;
+
   final Function() onSelect;
   final bool isSelected;
   final Color? borderColor;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        boxShadow: isSelected
-            ? [
-                BoxShadow(
-                  color: Colors.blue.withOpacity(0.27),
-                  blurRadius: 30,
-                  offset: Offset(3.w, 2.h), // changes position of shadow
-                ),
-              ]
-            : [],
-      ),
-      child: ChoiceChip(
-        label: Text(text),
-        selected: isSelected,
-        onSelected: (bool selected) {
-          onSelect();
-        },
-        selectedColor:
-            AppLightThemeColors.kMainBlueButton, // Color for the selected chip
-        backgroundColor:
-            AppLightThemeColors.kMainLightButton, // Color for unselected chips
-        labelStyle: TextStyle(
-          color:
-              isSelected ? Colors.white : AppLightThemeColors.kLightTextColor,
-          fontSize: 17.sp,
-          fontFamily: "Nunito",
+    return Padding(
+      padding:  EdgeInsets.all(2.w),
+      child: Container(
+        decoration: BoxDecoration(
+          boxShadow: isSelected
+              ? [
+                  BoxShadow(
+                    color: Colors.blue.withOpacity(0.27),
+                    blurRadius: 30,
+                    offset: Offset(3.w, 2.h), // changes position of shadow
+                  ),
+                ]
+              : [],
         ),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20.sp),
-        ),
-        padding: EdgeInsets.symmetric(horizontal: 3.8.w, vertical: 1.6.w),
-        showCheckmark: false,
-        side: BorderSide(
-          color: borderColor ?? AppLightThemeColors.kMainLightButtonBorder,
+        child: ChoiceChip(
+          label: Text(text),
+          selected: isSelected,
+          onSelected: (bool selected) {
+            onSelect();
+          },
+          selectedColor:
+              AppLightThemeColors.kMainBlueButton, // Color for the selected chip
+          backgroundColor:
+              AppLightThemeColors.kMainLightButton, // Color for unselected chips
+          labelStyle: TextStyle(
+            color:
+                isSelected ? Colors.white : AppLightThemeColors.kLightTextColor,
+            fontSize: 17.sp,
+            fontFamily: "Nunito",
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.sp),
+          ),
+          padding: EdgeInsets.symmetric(horizontal: 3.8.w, vertical: 1.6.w),
+          showCheckmark: false,
+          side: BorderSide(
+            color: borderColor ?? AppLightThemeColors.kMainLightButtonBorder,
+          ),
         ),
       ),
     );
