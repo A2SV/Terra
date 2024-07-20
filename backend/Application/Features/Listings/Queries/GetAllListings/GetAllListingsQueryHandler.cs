38f2b72316a8878c562938e4394c824e50e4ab39
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2024-07-20 23:16:16 +0000

    feat(backend) : Add GetAlllisting Command and Handler

---
+++ backend/Application/Features/Listings/Queries/GetAllListings/GetAllListingsQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using Application.Contracts;
+using Domain.Entities;
+using Domain.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Listings.Queries.GetAllListings
+{
+    public class GetAllListingQueryHandler : IRequestHandler<GetAllListingQuery, PaginatedList<Property>>
+    {
+        private readonly IListingRepository _listingRepository;
+
+        public GetAllListingQueryHandler(IListingRepository listingRepository)
+        {
+            _listingRepository = listingRepository;
+        }
+
+        public async Task<PaginatedList<Property>> Handle(GetAllListingQuery request, CancellationToken cancellationToken)
+        {
+            return await _listingRepository.GetAllListings(request.pageIndex, request.pageSize,
+                                                            request.propertyType, request.minPrice,
+                                                            request.maxPrice, request.bedrooms,
+                                                            request.bathrooms, request.propertiesFacilities,
+                                                            request.studentAccommodation);
+        }
+
+    }
+}
