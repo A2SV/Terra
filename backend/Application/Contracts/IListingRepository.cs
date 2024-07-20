3a93beaeb2b29099b6ada9b7b8f1841298ab5ef2
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2024-07-20 23:15:53 +0000

    feat(backend) : Add GetAllListing method

--- backend/Application/Contracts/IListingRepository.cs
+++ backend/Application/Contracts/IListingRepository.cs
@@ -1,12 +1,22 @@
 using Domain.Entities;
+using Domain.Models;
 
 namespace Application.Contracts
 {
-    public interface IListingRepository
+    public interface IListingsRepository
     {
         Task SaveChangesAsync();
         Task <TEntity?> AddPropertyAsync<TEntity>(TEntity property) where TEntity : class;
         Task<PropertyLocation?> AddPropertyLocationAsync(PropertyLocation propertyLocation);
         Task<PaymentInformation?> AddPaymentInformationAsync(PaymentInformation paymentInformation);
+        Task<PaginatedList<Property>> GetAllListings(int pageIndex,
+            int pageSize,
+            string propertyType = null,
+            int? minPrice = null,
+            int? maxPrice = null,
+            int? bedrooms = null,
+            int? bathrooms = null,
+            string? propertiesFacilities = null,
+            string studentAccommodation = null);
     }
 }
