5347f7fe8a66f10f97f008be4144d4c62fdd0f99
Haqq Bassit <munagahmnh@gmail.com>
2024-07-21 22:00:15 +0000

    (fix): Add foreign keys

--- backend/Domain/Entities/Shop.cs
+++ backend/Domain/Entities/Shop.cs
@@ -5,7 +5,11 @@ namespace Domain.Entities
 {
     public class Shop : BaseEntity
     {
+        [ForeignKey(nameof(CommercialProperty))]
+        public Guid CommercialPropertyId { get; set; }
         public bool DisplayWindowAvailable { get; set; }
         public double StorageRoomSize { get; set; }
+
+        public CommercialProperty CommercialProperty { get; set; } = null!;
     }
 }
