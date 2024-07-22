89720f90e98af5dd24d358cf2dbc349f1ef7dfc1
lit <103055119+EricaAnnor@users.noreply.github.com>
2024-07-22 20:26:16 +0000

    Ggpp 198 design home page (#132)
    * feat(web): stage
    
    * feat(web): integrate all components for the homepage
    
    * feat(web):Design homepage

--- web/components/General/Navbar.tsx
+++ web/components/General/Navbar.tsx
@@ -3,6 +3,8 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import HamburgerMenu from "./Hamburger";
+import { ProfilePic } from "./Profile";
+import { useSession } from "next-auth/react";
 
 const Navbar = () => {
   const [activeItem, setActiveItem] = useState("Home");
@@ -11,19 +13,22 @@ const Navbar = () => {
     setActiveItem(item);
   };
 
+  const { data: session } = useSession();
+  const userIsLoggedIn = !!session;
+
   return (
-    <div>
-      <div className="flex flex-row justify-between w-full h-16 py-2 lg:px-8 px-4 pt-2">
-        <div>
-          <Image src="/TERRA.svg" alt="Logo" height={64} width={70} className="object-cover" />
+    <div className="pt-3 fixed top-0 z-50 w-full bg-white pb-[16px]">
+      <div className="flex flex-row justify-between w-full h-16 py-2 lg:px-8 px-4 pt-4 ">
+        <div className="flex items-center ">
+          <Image src="/headerLogo.svg" alt="Logo" height={90} width={80} className="object-cover" />
         </div>
-        <div className="lg:flex flex-row items-end hidden ">
+        <div className="lg:flex flex-row items-center hidden ">
           <nav>
             <ul className="flex flex-row">
               {["Home", "Search", "Student", "About us", "Contact us"].map((item) => (
                 <li
                   key={item}
-                  className={`px-4  ${activeItem === item ? "text-[#03045E]" : "text-black"}`}
+                  className={`px-3 font-nunito ${activeItem === item ? "text-[#03045E]" : "text-black"}`}
                   onClick={() => handleItemClick(item)}
                 >
                   {item}
@@ -33,20 +38,30 @@ const Navbar = () => {
           </nav>
         </div>
         <div className="flex flex-row">
-          <div className="flex flex-row  ">
-            <Image
-              src="/header_image.jpg"
-              alt="avatar"
-              height={50}
-              width={50}
-              className="object-cover  rounded-full"
-            />
-            <p className="lg:flex hidden items-center px-4">
-              Daniel Shimelis <span className="text-[24px] pl-3"> &#124;</span>
-            </p>
-          </div>
+          {userIsLoggedIn ? (
+            <div className="flex flex-row">
+              <ProfilePic />
+              <p className="lg:flex hidden items-center px-4 font-nunito">
+                Daniel Shimelis <span className="text-[20px] pl-3"> &#124;</span>
+              </p>
+            </div>
+          ) : (
+            <div className="flex flex-row">
+              <Image
+                src="/profilePic.svg"
+                alt="login"
+                height={40}
+                width={40}
+                className="object-cover"
+              />
+              <p className="lg:flex hidden items-center px-4 font-nunito">
+                Login <span className="text-[20px] pl-3"> &#124;</span>
+              </p>
+            </div>
+          )}
+
           <div className="lg:flex hidden items-center">
-            <button className="bg-[#1779F3] text-white font-semibold text-[13px] px-7 py-3 rounded-[25px] ">
+            <button className="bg-[#1779F3] text-white font-nunito font-semibold text-[13px] px-5 py-3 rounded-[25px] ">
               Custom Listing
             </button>
           </div>
