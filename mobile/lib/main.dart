b3e66e53a7ae2f8be14fad54d6dc508c115a29ff
KudusMusah <118946022+KudusMusah@users.noreply.github.com>
2024-07-22 20:19:10 +0000

    feat(mobile): [test] checking if there's a change (#133)

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -51,7 +51,7 @@ class MainApp extends StatelessWidget {
             darkTheme: ThemeConfig().darkTheme,
             theme: ThemeConfig().lightTheme,
             themeMode: ThemeMode.light,
-          ),
+          ), // checking if there's a change
         );
       },
     );
