190c09f09a8cf3c10f26077a62e1c4823f1dd204
Ebenezer Acquah <71809927+Eben-Success@users.noreply.github.com>
2024-07-22 21:49:57 +0000

    remove token field from ResetPassword (#136)

--- backend/Application/Features/Accounts/ResetPassword/ResetPasswordCommand.cs
+++ backend/Application/Features/Accounts/ResetPassword/ResetPasswordCommand.cs
@@ -6,14 +6,12 @@ namespace Application.Features.Accounts.ResetPassword;
 public class ResetPasswordCommand : IRequest<Result>
 {
     public string Email { get; set; }
-    public string Token { get; set; }
     public string NewPassword { get; set; }
     public string ConfirmPassword { get; set; }
 
-    public ResetPasswordCommand(string email, string token, string newPassword, string confirmPassword)
+    public ResetPasswordCommand(string email, string newPassword, string confirmPassword)
     {
         Email = email;
-        Token = token;
         NewPassword = newPassword;
         ConfirmPassword = confirmPassword;
     }
