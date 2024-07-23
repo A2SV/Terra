a4af8c63fc9250d23e55a19e082b4f7d988cf21e
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2024-07-23 23:52:10 +0000

    feat(backend) : Add constructor

--- backend/Application/Features/Listings/Queries/Filtering/FilterCommandHandler.cs
+++ backend/Application/Features/Listings/Queries/Filtering/FilterCommandHandler.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Application.Contracts;
+using Application.Features.Listings.Queries.GetAllListings;
+using Domain.Entities;
+using Domain.Models;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,23 @@ using System.Threading.Tasks;
 
 namespace Application.Features.Listings.Queries.Filtering
 {
-    public class FilterCommandHandler
+    public class FilterCommandHandler : IRequestHandler< FilterCommand, PaginatedList<Property>>
     {
+        private readonly IListingRepository _listingRepository;
+
+        public FilterCommandHandler(IListingRepository listingRepository)
+        {
+            _listingRepository = listingRepository;
+        }
+
+        public async Task<PaginatedList<Property>> Handle(FilterCommand request, CancellationToken cancellationToken)
+        {
+            return await _listingRepository.Filter(request.pageIndex, request.pageSize,
+                                                   request.listingType, request.propertyType,
+                                                   request.subType,request.minPrice,
+                                                   request.maxPrice, request.priceFrequency, 
+                                                   request.minPropertySize, request.maxPropertySize,
+                                                   request.amenities);
+        }
     }
 }
