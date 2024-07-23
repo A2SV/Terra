34b14b6461ea476e7f8dddd78c027a06da973579
Raphael Adjetey Adjei <100845372+RAAdjei@users.noreply.github.com>
2024-07-23 19:51:54 +0000

    feat(web): Designed the hero section of the detailed listing information page (#141)

---
+++ web/app/heroListing/page.tsx
@@ -0,0 +1,12 @@
+import HeroListing from "@/components/HeroListing/HeroListing";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <HeroListing />
+    </div>
+  );
+};
+
+export default page;
