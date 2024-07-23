30570e3ae41d7b2687e678b21519cb9b2321e9b8
sedfarouk <sadikalhanssah@gmail.com>
2024-07-23 21:57:23 +0000

    run initial migrations

---
+++ backend/Persistence/Models/Hotel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Persistence.Models;
+
+public partial class Hotel
+{
+    public Guid Id { get; set; }
+
+    public int StarRating { get; set; }
+
+    public bool RestaurantOnSite { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+}
