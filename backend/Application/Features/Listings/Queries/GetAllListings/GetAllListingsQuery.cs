3684159f13d87b659de4c119f2adfff8a886f193
Michael Adu-Gyamfi <119356822+mikkayadu@users.noreply.github.com>
2024-07-24 20:26:54 +0000

    Ggpp 193 implement get all listing functionality (#154)
    * feat(backend) : Add GetAllListing method
    
    * feat(backend) : Add GetAlllisting Command and Handler
    
    * feat(backend) : Add GetAllListing endpoint
    
    * feat(backend) : Modify ListingRepository
    
    * feat(backend) : Modify filter for StundentAcoomodationLocation
    
    * fix(backend) : Separate Filtering From GetAllListing
    
    * feat(backend) : Implement Filtering
    
    * feat(backend) : Implement filtering
    
    * feat(backend) : Add constructor
    
    * feat(backend) : Fix filter endpoint
    
    * feat(backend) : Add constructor to the FilterCommand
    
    * feat(backend) : Change file names
    
    * fix(backend) : Delete .filenesting.json
    
    * fix(backend) : Delete .filenesting.json
    
    * fix(backend) : Delete ListingsController
    
    * feat(backend) : Delete old files

---
+++ backend/Application/Features/Listings/Queries/GetAllListings/GetAllListingsQuery.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Entities;
+using Domain.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Listings.Queries.GetAllListings
+{
+    public class GetAllListingQuery : IRequest<PaginatedList<Property>>
+    {
+        public int pageIndex { get; set; }
+        public int pageSize { get; set; }
+
+
+        public GetAllListingQuery(int PageIndex, int PageSize )
+        {
+            pageIndex = PageIndex;
+            pageSize = PageSize;
+
+        }
+    }
+}
