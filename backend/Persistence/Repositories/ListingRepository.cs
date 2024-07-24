592c482ec40fdb1dd32e60186ce9d9253a4e06c4
Michael Adu-Gyamfi <119356822+mikkayadu@users.noreply.github.com>
2024-07-24 20:26:54 +0000

    Ggpp 193 implement get all listing functionality (#154)
    * feat(backend) : Add GetAllListing method
    
    * feat(backend) : Add GetAlllisting Command and Handler
    
    * feat(backend) : Add GetAllListing endpoint
    
    * feat(backend) : Modify ListingRepository
    
    * feat(backend) : Modify filter for StundentAcoomodationLocation
    
    * fix(backend) : Separate Filtering From GetAllListing
    
    * feat(backend) : Implement Filtering
    
    * feat(backend) : Implement filtering
    
    * feat(backend) : Add constructor
    
    * feat(backend) : Fix filter endpoint
    
    * feat(backend) : Add constructor to the FilterCommand
    
    * feat(backend) : Change file names
    
    * fix(backend) : Delete .filenesting.json
    
    * fix(backend) : Delete .filenesting.json
    
    * fix(backend) : Delete ListingsController
    
    * feat(backend) : Delete old files

--- backend/Persistence/Repositories/ListingRepository.cs
+++ backend/Persistence/Repositories/ListingRepository.cs
@@ -1,5 +1,7 @@
 using Application.Contracts;
 using Domain.Entities;
+using Domain.Enums;
+using Domain.Models;
 using Microsoft.EntityFrameworkCore;
 using Persistence.Configurations;
 
@@ -12,7 +14,7 @@ namespace Persistence.Repositories
         {
             _context = context;
         }
-        
+
         public async Task SaveChangesAsync()
         {
             await _context.SaveChangesAsync(CancellationToken.None);
@@ -38,5 +40,117 @@ namespace Persistence.Repositories
             if (result == null) return null;
             return paymentInformation;
         }
+
+        public async Task<PaginatedList<Property>> GetAllListings(int pageIndex, int pageSize)
+        {
+
+            IQueryable<Property> query = _context.Properties;
+
+
+            var count = await query.CountAsync();
+
+
+            var properties = await query
+                .OrderBy(p => p.CreatedAt)
+                .Skip((pageIndex - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            var totalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+
+            return new PaginatedList<Property>(properties, pageIndex, totalPages);
+        }
+
+
+        public async Task<PaginatedList<Property>> Filter(
+            int pageIndex, int pageSize,
+            string? listingType, string? propertyType,
+            string? subType, int? minPrice,
+            int? maxPrice, string? paymentFrequency,
+            int? minPropertySize, int? maxPropertySize,
+            string? amenities
+                )
+        {
+
+            IQueryable<Property> query = _context.Properties;
+
+
+            if (!string.IsNullOrEmpty(listingType) && Enum.TryParse<PropertyListingType>(listingType, out var parsedListingType))
+            {
+                query = query.Where(x => x.ListingType == parsedListingType);
+            }
+
+            if (!string.IsNullOrEmpty(propertyType))
+                if (propertyType == "Residential")
+                {
+                    query = query.Where(p => (int)p.PropertyType % 2 == 0);
+                }
+                else
+                {
+                    query = query.Where(p => (int)p.PropertyType % 2 != 0);
+                }
+
+
+            if (!string.IsNullOrEmpty(subType) && Enum.TryParse<PropertyType>(subType, out var parsedSubType))
+            {
+                query = query.Where(x => x.PropertyType == parsedSubType);
+
+            }
+
+            if (minPrice.HasValue || maxPrice.HasValue)
+            {
+                query = query.
+                    Include(p => p.PaymentInformation)
+                    .Where(x => (!minPrice.HasValue || x.PaymentInformation.Cost >= minPrice.Value) &&
+                                (!maxPrice.HasValue || x.PaymentInformation.Cost <= maxPrice.Value));
+            }
+
+
+
+            if (!string.IsNullOrEmpty(subType) && Enum.TryParse<PaymentFrequency>(paymentFrequency, out var parsedPaymentFrequency))
+            {
+                query = query.
+                        Include(p => p.PaymentInformation)
+                        .Where(x => x.PaymentInformation.PaymentFrequency == parsedPaymentFrequency);
+
+
+            }
+
+
+            if (minPropertySize.HasValue || maxPropertySize.HasValue)
+            {
+                query = query
+                    .Where(x => (!minPropertySize.HasValue || x.PropertySize >= minPropertySize.Value) &&
+                                (!maxPrice.HasValue || x.PropertySize <= maxPrice.Value));
+            }
+
+
+            if (!string.IsNullOrEmpty(amenities))
+            {
+                query = query.Join(_context.PropertyAmenities,
+                    property => property.Id,
+                    propertyAmenity => propertyAmenity.PropertyId,
+                    (property, propertyAmenity) => new { Property = property, PropertyAmenity = propertyAmenity })
+                    .Where(x => (x.PropertyAmenity.Amenity.Name == amenities))
+                    .Select(x => x.Property);
+            }
+
+            var count = await query.CountAsync();
+
+
+            var properties = await query
+                .OrderBy(p => p.CreatedAt)
+                .Skip((pageIndex - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            var totalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+
+            return  new PaginatedList<Property>(properties, pageIndex, totalPages);
+            }
+
     }
 }
+
