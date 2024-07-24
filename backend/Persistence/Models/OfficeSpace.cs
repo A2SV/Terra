684fb4bf81d64198f421263ebb15d2f6ed43b230
Ohene-Adjei George Kofi <georgeey123@gmail.com>
2024-07-24 00:07:02 +0000

    respecify path to google credentials (#149)
    * feat(backend): implement file upload
    
    * feat(backend): add google credentials and service injection
    
    * fix(backend): remove google credentials
    
    * feat(backend): fix add working google credentials
    
    * fix(backend): Fix duplicate member initiation and correct namespace declaratin
    
    * fix(backend): remove [FromFile] attribute
    
    * fix(backend): add google credentials to docker image
    
    * fix(backend): respecify path of google credential
    
    * feat(backend) : Add GetAllListing method
    
    * feat(backend) : Add GetAlllisting Command and Handler
    
    * feat(backend) : Add GetAllListing endpoint
    
    * feat(backend) : Modify ListingRepository
    
    * feat(backend) : Modify filter for StundentAcoomodationLocation
    
    * feat(backend): implement get listings with search, filter, pagination and sort
    
    * run initial migrations
    
    * remove migrations folder from gitignore
    
    ---------
    
    Co-authored-by: mnhaqq <118978306+mnhaqq@users.noreply.github.com>
    Co-authored-by: Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
    Co-authored-by: sedfarouk <sadikalhanssah@gmail.com>

---
+++ backend/Persistence/Models/OfficeSpace.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Persistence.Models;
+
+public partial class OfficeSpace
+{
+    public Guid Id { get; set; }
+
+    public int OfficeSpaceType { get; set; }
+
+    public bool MeetingRoomsAvailable { get; set; }
+
+    public bool ReceptionAreaAvailable { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+}
