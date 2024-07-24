adaa412b588375e7e414f7af3bfeab980431c789
Al-Waasiu Abubakari <99828747+FARAD56@users.noreply.github.com>
2024-07-24 21:32:09 +0000

    WIP: 7330f3c Ggpp 211 compile all components (#152) (#161)

--- mobile/lib/src/features/dashboard/presentation/pages/dashboard.dart
+++ mobile/lib/src/features/dashboard/presentation/pages/dashboard.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
-import 'package:mobile/src/core/dp_injection/dependency_injection.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:mobile/src/features/dashboard/presentation/pages/homepage.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 
 class DashBoard extends StatefulWidget {
   const DashBoard({super.key});
@@ -12,23 +13,106 @@ class DashBoard extends StatefulWidget {
 class _DashBoardState extends State<DashBoard> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text("Dashboard"),
-          ElevatedButton(
-              onPressed: () async {
-                final Box userBox = sl();
-                userBox.put('isLoggedIn', false);
-                userBox.delete('username');
-                userBox.delete('password');
-                print('logged in ? ${userBox.get('isLoggedIn')}');
-                Navigator.pushReplacementNamed(context, '/signin');
-              },
-              child: const Text('Logout')),
-        ],
+    return SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.white,
+      body: const HomePage(),
+      bottomNavigationBar: BottomAppBar(
+        elevation: 10,
+        surfaceTintColor: Colors.white,
+        shadowColor: Colors.black,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Column(
+              children: [
+                SizedBox(
+                  height: 0.5.h,
+                ),
+                SvgPicture.asset(
+                  'assets/svg/home.svg',
+                  height: 20.sp,
+                ),
+                SizedBox(
+                  height: 0.3.h,
+                ),
+                Text(
+                  'Home',
+                  style:
+                      TextStyle(fontSize: 15.sp, fontWeight: FontWeight.w500),
+                )
+              ],
+            ),
+            Column(
+              children: [
+                SizedBox(
+                  height: 0.5.h,
+                ),
+                SvgPicture.asset(
+                  'assets/svg/message-text.svg',
+                  height: 20.sp,
+                ),
+                SizedBox(
+                  height: 0.3.h,
+                ),
+                Text(
+                  'Chat',
+                  style:
+                      TextStyle(fontSize: 15.sp, fontWeight: FontWeight.w500),
+                )
+              ],
+            ),
+            Column(
+              children: [
+                SizedBox(
+                  height: 0.5.h,
+                ),
+                Image.asset('assets/images/image6.png'),
+                SizedBox(
+                  height: 0.3.h,
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                SizedBox(
+                  height: 0.5.h,
+                ),
+                SvgPicture.asset(
+                  'assets/svg/tag.svg',
+                  height: 20.sp,
+                ),
+                SizedBox(
+                  height: 0.3.h,
+                ),
+                Text(
+                  'Saved',
+                  style:
+                      TextStyle(fontSize: 15.sp, fontWeight: FontWeight.w500),
+                )
+              ],
+            ),
+            Column(
+              children: [
+                SizedBox(
+                  height: 0.5.h,
+                ),
+                SvgPicture.asset(
+                  'assets/svg/profile.svg',
+                  height: 20.sp,
+                ),
+                SizedBox(
+                  height: 0.3.h,
+                ),
+                Text(
+                  'Profile',
+                  style:
+                      TextStyle(fontSize: 15.sp, fontWeight: FontWeight.w500),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     ));
   }
