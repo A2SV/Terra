f705454c64a3257d4758d63a8eeb1ad1c740c2be
Akua Serwaa Nkrumah <nkrumahakua2002@gmail.com>
2024-07-24 19:49:15 +0000

    feat(web): created the listdesc page:description-youtube-video,wrote tests

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,10 +1,16 @@
 import HomePage from "@/components/General/HomePage";
 import React from "react";
+// import ResultsListBackgroundImage from "../components/Common/Reusable/ResultsListBackgroundImage";
+// import AdvancedListingSettings from "../components/Common/Reusable/AdvancedListingSettings";
+import ListDec from "@/components/Listing/ListDesc";
 
 const Home: React.FC = () => {
   return (
     <div>
+      {/* <ResultsListBackgroundImage />
+      <AdvancedListingSettings /> */}
       <HomePage />
+      <ListDec />
     </div>
   );
 };
