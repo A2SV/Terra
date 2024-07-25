d3427961c1bf1aebe57fc2440e9e909b79102ea4
Michael Adu-Gyamfi <119356822+mikkayadu@users.noreply.github.com>
2024-07-25 22:24:40 +0000

    Ggpp 193 implement get all listing functionality (#172)
    * feat(backend) : Add GetAllListing method
    
    * feat(backend) : Add GetAlllisting Command and Handler
    
    * feat(backend) : Add GetAllListing endpoint
    
    * feat(backend) : Modify ListingRepository
    
    * fix(backend) : Separate Filtering From GetAllListing
    
    * feat(backend) : Implement Filtering
    
    * feat(backend) : Implement filtering
    
    * feat(backend) : Add constructor
    
    * feat(backend) : Add constructor to the FilterCommand
    
    * fix(backend) : Delete .filenesting.json
    
    * fix(backend) : Delete .filenesting.json
    
    * fix(backend) : Delete ListingsController
    
    * feat(backend) : Delete old files
    
    * fix(backend) : Change condition for PropertyType
    
    * feat(backend) : Change amenities to list
    
    * feat(backend) : Add navigation property of Residential and Commerical properties
    
    * feat(backend) : Change amenities to list
    
    * feat(backend) : Add authorization to endpoints
    
    * feat(backend) : Include Residential and Commercial
    
    * feat(backend) : Add authorization
    
    * feat(backend) : Remove authorization
    
    * feat(backend) : Remove authorization

--- backend/WebApi/Controllers/AmenityController.cs
+++ backend/WebApi/Controllers/AmenityController.cs
@@ -18,7 +18,9 @@ namespace WebApi.Controllers
             _mediator = mediator;
         }
 
-        [Authorize]
+
+        //[Authorize]
+
         [HttpGet]
         public async Task<IActionResult> GetAll([FromQuery] string? filterOn, [FromQuery] string? filterQuery,
                 [FromQuery] string? sortBy, [FromQuery] bool? isAscending)
