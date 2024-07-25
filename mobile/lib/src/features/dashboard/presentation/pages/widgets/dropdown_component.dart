db7ce6ed9d526c7e9014f1ee08917997b9c850ae
KudusMusah <118946022+KudusMusah@users.noreply.github.com>
2024-07-25 21:13:27 +0000

    feat(mobile): Create the listings filter page (#169)

--- mobile/lib/src/features/dashboard/presentation/pages/widgets/dropdown_component.dart
+++ mobile/lib/src/features/dashboard/presentation/pages/widgets/dropdown_component.dart
@@ -7,43 +7,50 @@ class DropdownComponent extends StatelessWidget {
   final List<Widget> tiles;
   final String title;
   final String trail;
-  const DropdownComponent({super.key, this.tiles = const [],required this.title,this.trail = ""});
+  final double rightPadding;
+  const DropdownComponent(
+      {super.key,
+      this.tiles = const [],
+      required this.title,
+      this.trail = "",
+      this.rightPadding = 0});
 
   @override
   Widget build(BuildContext context) {
     return Theme(
-      data: ThemeData().copyWith(dividerColor: Colors.transparent),
+      data: ThemeData().copyWith(
+        dividerColor: Colors.transparent,
+        splashColor: Colors.transparent,
+      ),
       child: Padding(
-        padding:  EdgeInsets.symmetric(vertical: 1.h),
+        padding: EdgeInsets.symmetric(vertical: 1.h),
         child: ExpansionTile(
+          tilePadding: EdgeInsets.only(right: rightPadding),
           clipBehavior: Clip.none,
           controlAffinity: ListTileControlAffinity.platform,
-          enableFeedback: false,
-          collapsedBackgroundColor: AppLightThemeColors.kFieldBackgroundColor,
           expandedAlignment: Alignment.centerLeft,
-        
-          title: Row( mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              
               Text(
                 title,
-                style: context.textTheme.titleMedium!.copyWith(color: Colors.black,
-                fontSize: 17.sp,fontWeight: FontWeight.w600),
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 17.sp,
+                ),
               ),
-        
               Text(
                 trail,
-                style: context.textTheme.bodySmall!.copyWith(color: AppLightThemeColors.kMainBlueButton,
-                fontSize: 14.sp,fontWeight: FontWeight.w600),
+                style: context.textTheme.bodySmall!.copyWith(
+                    color: AppLightThemeColors.kMainBlueButton,
+                    fontSize: 14.sp,
+                    fontWeight: FontWeight.w600),
               ),
-          
             ],
-        
           ),
-        
           children: [
-            Wrap(clipBehavior: Clip.none,
-              children: tiles),
+            Wrap(clipBehavior: Clip.none, children: tiles),
           ],
         ),
       ),
--- mobile/lib/src/features/dashboard/presentation/widgets/filter_button.dart
+++ mobile/lib/src/features/dashboard/presentation/widgets/filter_button.dart
@@ -9,18 +9,22 @@ class FilterButton extends StatelessWidget {
     required this.onSelect,
     required this.isSelected,
     this.borderColor,
+    this.size = 17,
+    this.hasShadow = true,
   });
 
   final String text;
   final Function() onSelect;
   final bool isSelected;
   final Color? borderColor;
+  final double size;
+  final bool hasShadow;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        boxShadow: isSelected
+        boxShadow: isSelected & hasShadow
             ? [
                 BoxShadow(
                   color: Colors.blue.withOpacity(0.27),
@@ -43,13 +47,13 @@ class FilterButton extends StatelessWidget {
         labelStyle: TextStyle(
           color:
               isSelected ? Colors.white : AppLightThemeColors.kLightTextColor,
-          fontSize: 17.sp,
+          fontSize: size.sp,
           fontFamily: "Nunito",
         ),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20.sp),
         ),
-        padding: EdgeInsets.symmetric(horizontal: 3.8.w, vertical: 1.6.w),
+        padding: EdgeInsets.symmetric(horizontal: 3.w, vertical: 1.6.w),
         showCheckmark: false,
         side: BorderSide(
           color: borderColor ?? AppLightThemeColors.kMainLightButtonBorder,
