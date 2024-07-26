3fe82af6f7f16622c82e5bd01f1f4bbe39424cc9
tibebugwas <138916342+tibebugwas@users.noreply.github.com>
2024-07-26 13:25:26 +0000

    Tibebu.deployment (#187)
    * feat(web): env issue
    
    * feat(web): env issue
    
    ---------
    
    Co-authored-by: Tibebu <tibebugwas@gmail.com>

--- web/utils/Fetch_Token.ts
+++ web/utils/Fetch_Token.ts
@@ -1,5 +1,6 @@
 "use client";
 import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { env } from "next-runtime-env";
 
 const fetchWithToken = async (args: any, api: any, extraOptions: any) => {
   const token = sessionStorage.getItem("token");
@@ -9,7 +10,7 @@ const fetchWithToken = async (args: any, api: any, extraOptions: any) => {
   }
 
   return fetchBaseQuery({
-    baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
+    baseUrl: env("NEXT_PUBLIC_BASE_URL"),
     prepareHeaders: (headers) => {
       if (token) {
         headers.set("authorization", `Bearer ${token}`);
