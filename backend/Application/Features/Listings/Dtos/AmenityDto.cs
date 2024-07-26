a9a5263536119fe939bcd942f4d302af9204b2b1
mnhaqq <118978306+mnhaqq@users.noreply.github.com>
2024-07-26 12:00:03 +0000

    Fix null values in get all listing functionality (#183)
    * (fix): Include payment information and property location in get all listings
    
    * (fix): Add media to get all listing
    
    * (fix): Add correct type in controller
    
    * (feat): create functionality for get listing by id

---
+++ backend/Application/Features/Listings/Dtos/AmenityDto.cs
@@ -0,0 +1,9 @@
+namespace Application.Features.Listings.Dtos
+{
+    public class AmenityDto
+    {
+        public required string AmenityName {get; set;}
+        public DateTime CreatedAt {get; set;}
+        public DateTime UpdatedAt {get; set;}
+    }
+}
---
+++ backend/Application/Features/Listings/Dtos/HouseDto.cs
@@ -0,0 +1,9 @@
+namespace Application.Features.Listings.Dtos
+{
+    public class HouseDto
+    {
+        public int NumberOfStories { get; set; }
+        public double GarageSpace { get; set; }
+        public bool StudentFriendly { get; set; }
+    }
+}
