e251ffa26f5ece377ab5f5e8a5c04f93818f7714
lit <103055119+EricaAnnor@users.noreply.github.com>
2024-07-26 11:14:43 +0000

    feat(web): create the advanced-filter page

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,12 +1,10 @@
-import Filters from "@/components/General/Filters";
-// import HomePage from "@/components/General/HomePage";
+import HomePage from "@/components/General/HomePage";
 import React from "react";
 
 const Home: React.FC = () => {
   return (
     <div>
-      {/* <HomePage /> */}
-      <Filters />
+      <HomePage />
     </div>
   );
 };
