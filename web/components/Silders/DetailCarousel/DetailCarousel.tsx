468c053859545f974913cc3472459e2dd70c0a19
Tawfick Mohammed Kassim <tawfickkassimmohammed@gmail.com>
2024-10-16 19:34:01 +0000

    feat(web): Corrected Error Fetching Listing

--- web/components/Silders/DetailCarousel/DetailCarousel.tsx
+++ web/components/Silders/DetailCarousel/DetailCarousel.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { EmblaOptionsType } from "embla-carousel";
-import "@/components/DetailCarousel/css/embla.css";
-import "@/components/DetailCarousel/css/sandbox.css";
+import "@/components/Silders/DetailCarousel/css/embla.css";
+import "@/components/Silders/DetailCarousel/css/sandbox.css";
 import EmblaCarousel from "@/components/Silders/ImageCarousel2/EmblaCarousel";
 
 interface Props {
