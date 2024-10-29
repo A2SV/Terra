36767faecb6fb80d62e241361847705f1e2bc8b1
greatnessmensah <ephraimmensah99@gmail.com>
2024-10-29 14:11:16 +0000

    fixed text alignment on ipad view

--- web/components/ContactUs/ContactUsHero.tsx
+++ web/components/ContactUs/ContactUsHero.tsx
@@ -13,7 +13,7 @@ const ContactUsHero = () => {
             quality={100}
             className="absolute inset-0 z-0"
           /> 
-          <div className="relative flex flex-col my-auto text-white w-7/12 md:w-5/12 pl-10 md:pl-28">
+          <div className="relative flex flex-col my-auto text-white w-7/12 lg:w-5/12 pl-10 md:pl-28">
             <div className="text-[32px] md:text-[56px] font-nunito font-bold">Contact Us</div>
             <div className="text-[12px] md:text-[24px] font-nunito mt-2">Need assistance? Reach out to us for prompt and friendly support</div>
           </div>
