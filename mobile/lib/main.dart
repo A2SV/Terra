2956d8da890a1884042b3bdcaeceee1799cd6688
MIYAKA <75727824+BambinoRiya@users.noreply.github.com>
2024-10-29 14:24:56 +0000

    fixed an error in main

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,60 +1,22 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:http/http.dart' as http;
-// import 'package:internet_connection_checker/internet_connection_checker.dart';
-// import 'package:mobile/src/core/dp_injection/dependency_injection.dart';
-// import 'package:mobile/src/core/network/network_info.dart';
-// import 'package:mobile/src/core/routes/routes_config.dart';
-// import 'package:mobile/src/features/auth/data/data_sources/auth_remote_data_source.dart';
-// import 'package:mobile/src/features/auth/data/repositories/auth_repository_impl.dart';
-// import 'package:mobile/src/features/auth/domain/use_cases/resend_otp.dart';
-// import 'package:mobile/src/features/auth/domain/use_cases/verify_otp.dart';
-// import 'package:mobile/src/features/auth/presentation/bloc/bloc/authentication_bloc.dart';
-// import 'package:mobile/src/features/auth/presentation/bloc/otp/otp_bloc.dart';
-// import 'package:mobile/src/features/auth/presentation/pages/otp_page.dart';
-// import 'package:responsive_sizer/responsive_sizer.dart';
-
-// Future<void> main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await init();
-//   runApp(const MainApp());
-// }
-
-// class MainApp extends StatelessWidget {
-//   const MainApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return ResponsiveSizer(
-//       builder: (context, orientation, screenType) {
-//         return MultiBlocProvider(providers: [
-//           BlocProvider<OTPBloc>(
-//             create: (context) => OTPBloc(
-//                 resendOtpUseCase: OTPResendUseCase(AuthRepositoryImpl(
-//                   remoteDataSource: AuthRemoteDataSourceImpl(http.Client()),
-//                   network: NetworkImpl(InternetConnectionChecker()),
-//                 )),
-//                 otpUseCase: OTPUseCase(AuthRepositoryImpl(
-//                   remoteDataSource: AuthRemoteDataSourceImpl(http.Client()),
-//                   network: NetworkImpl(InternetConnectionChecker()),
-//                 ))),
-//             child: const OTPage(),
-//           ),
-//           BlocProvider(
-//             create: (context) => sl<AuthenticationBloc>(),
-//           ),
-//         ], child: AppRouter());
-//       },
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
-import 'package:mobile/src/features/dashboard/presentation/pages/add_listing_res_hotel.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:http/http.dart' as http;
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:mobile/src/core/dp_injection/dependency_injection.dart';
+import 'package:mobile/src/core/network/network_info.dart';
+import 'package:mobile/src/core/routes/routes_config.dart';
+import 'package:mobile/src/features/auth/data/data_sources/auth_remote_data_source.dart';
+import 'package:mobile/src/features/auth/data/repositories/auth_repository_impl.dart';
+import 'package:mobile/src/features/auth/domain/use_cases/resend_otp.dart';
+import 'package:mobile/src/features/auth/domain/use_cases/verify_otp.dart';
+import 'package:mobile/src/features/auth/presentation/bloc/bloc/authentication_bloc.dart';
+import 'package:mobile/src/features/auth/presentation/bloc/otp/otp_bloc.dart';
+import 'package:mobile/src/features/auth/presentation/pages/otp_page.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
-
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await init();
   runApp(const MainApp());
 }
 
@@ -65,14 +27,25 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ResponsiveSizer(
       builder: (context, orientation, screenType) {
-        return MaterialApp(
-          title: 'Hotel Listing App',
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
+        return MultiBlocProvider(providers: [
+          BlocProvider<OTPBloc>(
+            create: (context) => OTPBloc(
+                resendOtpUseCase: OTPResendUseCase(AuthRepositoryImpl(
+                  remoteDataSource: AuthRemoteDataSourceImpl(http.Client()),
+                  network: NetworkImpl(InternetConnectionChecker()),
+                )),
+                otpUseCase: OTPUseCase(AuthRepositoryImpl(
+                  remoteDataSource: AuthRemoteDataSourceImpl(http.Client()),
+                  network: NetworkImpl(InternetConnectionChecker()),
+                ))),
+            child: const OTPage(),
           ),
-          home: const AddListingResHotel(), 
-        );
+          BlocProvider(
+            create: (context) => sl<AuthenticationBloc>(),
+          ),
+        ], child: AppRouter());
       },
     );
   }
 }
+
