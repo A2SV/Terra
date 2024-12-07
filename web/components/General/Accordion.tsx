60f32f8cf8f9f1e0785785f3894f4b960731b098
Tawfick Mohammed Kassim <tawfickkassimmohammed@gmail.com>
2024-12-07 03:27:12 +0000

    feat(web): final fixes for white spaces

--- web/components/General/Accordion.tsx
+++ web/components/General/Accordion.tsx
@@ -1,23 +1,24 @@
 "use client";
 
-import React, { useState, FC } from "react";
+import React, { FC } from "react";
 import { IoIosArrowDropdown, IoIosArrowDropup } from "react-icons/io";
+
 interface AccordionProps {
   title: string;
   answer: string;
+  isOpen: boolean;
+  onClick: () => void;
 }
 
-const Accordion: FC<AccordionProps> = ({ title, answer }) => {
-  const [accordionOpen, setAccordionOpen] = useState(false);
-
+const Accordion: FC<AccordionProps> = ({ title, answer, isOpen, onClick }) => {
   return (
     <div className="flex flex-col justify-center">
-      <button onClick={() => setAccordionOpen(!accordionOpen)} className="">
+      <button onClick={onClick} className="">
         <div className="flex h-20 pl-10 pr-7 justify-between items-center">
           <span className="font-montserrat font-light text-lg md:text-xl text-left text-black">
             {title}
           </span>
-          {accordionOpen ? (
+          {isOpen ? (
             <IoIosArrowDropup className="text-xl md:text-3xl shrink-0 text-terrablue mx-1 " />
           ) : (
             <IoIosArrowDropdown className="text-xl md:text-3xl shrink-0 text-terrablue mx-1" />
@@ -26,7 +27,7 @@ const Accordion: FC<AccordionProps> = ({ title, answer }) => {
 
         <div
           className={`grid overflow-hidden transition-all duration-300 ease-in-out text-slate-600 text-sm ${
-            accordionOpen ? "grid-rows-[1fr] opacity-100 mb-4" : "grid-rows-[0fr] opacity-0"
+            isOpen ? "grid-rows-[1fr] opacity-100 mb-4" : "grid-rows-[0fr] opacity-0"
           }`}
         >
           <div className="overflow-hidden font-montserrat font-light text-base md:text-lg text-gray-700 pl-10 pr-7 text-left">
--- web/components/General/FAQ.tsx
+++ web/components/General/FAQ.tsx
@@ -1,11 +1,14 @@
 import Accordion from "./Accordion";
-import FAQImage from "@/public/woman.svg";
+import FAQImage from "@/public/faq_woman.svg";
 import Image from "next/image";
+import { useState } from "react";
 
 const FAQ = () => {
+  const [activeAccordionIndex, setActiveAccordionIndex] = useState<number | null>(null);
+
   return (
     <div className="w-screen h-auto flex justify-center">
-      <div className="w-10/12 md:w-8/12 flex flex-col lg:flex-row lg:w-10/12">
+      <div className="w-10/12 md:w-8/12 flex flex-col xl:flex-row lg:w-10/12">
         <div className="order-1 lg:order-2 w-full mb-10 lg:mb-0 flex flex-col items-center lg:mr-10">
           <p className="text-lg text-terrablue font-montserrat mb-3 self-start">Got Questions</p>
           <h1 className="font-montserrat font-semibold text-gray-700 text-3xl mb-5 self-start">
@@ -20,31 +23,47 @@ const FAQ = () => {
           </div>
         </div>
 
-        <div className="order-2 lg:order-2 divide-y space-y-2 flex flex-col border w-full md:mt-14 overflow-hidden">
-          <Accordion
-            title="What types of properties can I list on Terra?"
-            answer="You can list a variety of properties on Terra, including apartments, houses, students' hostels, hotels, guest houses, office spaces, shops, warehouses, and event spaces."
-          />
-          <Accordion
-            title="How do I create a listing on Terra?"
-            answer="Creating a listing on Terra is straightforward. Simply sign up or log in to your account, navigate to the 'Create Listing' section, fill in the required property details, and upload high-quality photos."
-          />
-          <Accordion
-            title="How can I ensure my listing gets noticed?"
-            answer="To ensure your listing gets noticed, provide detailed and accurate descriptions, upload high-quality images, and consider using our premium listing options to boost visibility."
-          />
-          <Accordion
-            title="How can I get my property featured on the homepage?"
-            answer="To get your property featured on the homepage, you can opt for our premium listing service. This will increase the visibility of your property and attract more potential buyers or renters. This feature is currently under construction and will be available soon."
-          />
-          <Accordion
-            title=" Can I edit my listing after it's published?"
-            answer="Yes, you can edit your listing at any time. Simply log in to your account, go to your profile. Under 'My Listings,' select the listing you want to edit. Make the necessary changes and save them."
-          />
-          <Accordion
-            title="Is there a fee for listing a property on Terra?"
-            answer="No, currently there is no fee for listing a property on Terra. You can list your properties for free."
-          />
+        <div className="order-2 lg:order-2 divide-y space-y-2 flex flex-col border w-full md:mt-14">
+          {[
+            {
+              title: "What types of properties can I list on Terra?",
+              answer:
+                "You can list a variety of properties on Terra, including apartments, houses, students' hostels, hotels, guest houses, office spaces, shops, warehouses, and event spaces.",
+            },
+            {
+              title: "How do I create a listing on Terra?",
+              answer:
+                "Creating a listing on Terra is straightforward. Simply sign up or log in to your account, navigate to the 'Create Listing' section, fill in the required property details, and upload high-quality photos.",
+            },
+            {
+              title: "How can I ensure my listing gets noticed?",
+              answer:
+                "To ensure your listing gets noticed, provide detailed and accurate descriptions, upload high-quality images, and consider using our premium listing options to boost visibility.",
+            },
+            {
+              title: "How can I get my property featured on the homepage?",
+              answer:
+                "To get your property featured on the homepage, you can opt for our premium listing service. This will increase the visibility of your property and attract more potential buyers or renters. This feature is currently under construction and will be available soon.",
+            },
+            {
+              title: "Can I edit my listing after it's published?",
+              answer:
+                "Yes, you can edit your listing at any time. Simply log in to your account, go to your profile. Under 'My Listings,' select the listing you want to edit. Make the necessary changes and save them.",
+            },
+            {
+              title: "Is there a fee for listing a property on Terra?",
+              answer:
+                "No, currently there is no fee for listing a property on Terra. You can list your properties for free.",
+            },
+          ].map((item, index) => (
+            <Accordion
+              key={index}
+              title={item.title}
+              answer={item.answer}
+              isOpen={activeAccordionIndex === index}
+              onClick={() => setActiveAccordionIndex(index === activeAccordionIndex ? null : index)}
+            />
+          ))}
         </div>
       </div>
     </div>
