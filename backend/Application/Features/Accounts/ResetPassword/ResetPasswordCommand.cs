28b1c3c604c3e10fa74cb3be7cfa62e12e937ea3
Ohene-Adjei George Kofi <georgeey123@gmail.com>
2024-12-17 20:55:13 +0000

    backend(fix): reset password flow (#254)

--- backend/Application/Features/Accounts/ResetPassword/ResetPasswordCommand.cs
+++ backend/Application/Features/Accounts/ResetPassword/ResetPasswordCommand.cs
@@ -5,13 +5,13 @@ namespace Application.Features.Accounts.ResetPassword;
 
 public class ResetPasswordCommand : IRequest<Result>
 {
-    public string Email { get; set; }
+    public string Token { get; set; }
     public string NewPassword { get; set; }
     public string ConfirmPassword { get; set; }
 
-    public ResetPasswordCommand(string email, string newPassword, string confirmPassword)
+    public ResetPasswordCommand(string token, string newPassword, string confirmPassword)
     {
-        Email = email;
+        Token = token;
         NewPassword = newPassword;
         ConfirmPassword = confirmPassword;
     }
--- backend/Persistence/Repositories/TokenRepository.cs
+++ backend/Persistence/Repositories/TokenRepository.cs
@@ -44,5 +44,47 @@ namespace Persistence.Repositories
 
             return new JwtSecurityTokenHandler().WriteToken(token);
         }
+        
+        public ClaimsPrincipal? VerifyJwt(string token)
+        {
+            var Jwt_key = System.Environment.GetEnvironmentVariable("JWT_KEY");
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Jwt_key));
+            var jwt_issuer = System.Environment.GetEnvironmentVariable("JWT_ISSUER");
+            var jwt_audience = System.Environment.GetEnvironmentVariable("JWT_AUDIENCE");
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidIssuer = jwt_issuer,
+                ValidateAudience = true,
+                ValidAudience = jwt_audience,
+                ValidateLifetime = true,
+                IssuerSigningKey = key,
+                ValidateIssuerSigningKey = true
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            try
+            {
+                var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out var validatedToken);
+
+                // Ensure the token is a valid JWT
+                if (validatedToken is JwtSecurityToken jwtToken &&
+                    jwtToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    return principal;
+                }
+                else
+                {
+                    throw new SecurityTokenException("Invalid token");
+                }
+            }
+            catch (Exception)
+            {
+                // Log or handle token validation failure
+                return null;
+            }
+        }
     }
 }
