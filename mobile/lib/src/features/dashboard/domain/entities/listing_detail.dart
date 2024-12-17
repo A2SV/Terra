5e0105ca27dca7c8302bf429ba37f72789104d6a
serkhani <dak.boniface@gmail.com>
2024-12-17 19:46:57 +0000

    feat(mobile): added listing detail

--- mobile/lib/src/features/dashboard/domain/entities/listing_detail.dart
+++ mobile/lib/src/features/dashboard/domain/entities/listing_detail.dart
@@ -1,7 +1,6 @@
 import 'package:equatable/equatable.dart';
-import 'package:mobile/src/features/dashboard/data/models/media.dart';
-
-import 'entities.dart';
+import 'package:mobile/src/features/dashboard/domain/domain.dart';
+import 'package:mobile/src/features/dashboard/domain/entities/media.dart';
 
 class ListingDetailEntity extends Equatable {
   final String id;
@@ -9,20 +8,23 @@ class ListingDetailEntity extends Equatable {
   final String paymentInformationId;
   final String propertyLocationId;
   final String title;
-  final String description;
+  final String? description;
   final PropertyType propertyType;
   final PropertySubType propertySubType;
+  final PropertyPublishStatus publishStatus;
+  final PropertyMarketStatus marketStatus;
   final PropertyListingType listingType;
-  final PropertyPublishStatus propertyPublishStatus;
-  final PropertyMarketStatus propertyMarketStatus;
-  final String propertySize;
-  final DateTime availableStartDate;
-  final DateTime availableEndDate;
+  final double? propertySize;
+  final DateTime? availableStartDate;
+  final DateTime? availableEndDate;
   final Lister lister;
   final PaymentInformation paymentInformation;
   final PropertyLocation propertyLocation;
-  final List<MediaModel> propertyPhotos;
-  final List<MediaModel> propertyVideos;
+  final List<Media>? propertyPhotos;
+  final List<Media>? propertyVideos;
+  final List<Amenity>? propertyAmenities;
+  final CommercialProperty? commercialProperty;
+  final ResidentialProperty? residentialProperty;
 
   const ListingDetailEntity({
     required this.id,
@@ -30,21 +32,26 @@ class ListingDetailEntity extends Equatable {
     required this.paymentInformationId,
     required this.propertyLocationId,
     required this.title,
-    required this.description,
+    this.description,
     required this.propertyType,
+    required this.propertySubType,
+    this.publishStatus = PropertyPublishStatus.unpublished,
+    this.marketStatus = PropertyMarketStatus.unavailable,
     required this.listingType,
-    required this.propertyPublishStatus,
-    required this.propertyMarketStatus,
-    required this.propertySize,
-    required this.availableStartDate,
-    required this.availableEndDate,
+    this.propertySize,
+    this.availableStartDate,
+    this.availableEndDate,
     required this.lister,
     required this.paymentInformation,
     required this.propertyLocation,
-    required this.propertySubType,
-    required this.propertyPhotos,
-    required this.propertyVideos,
+    this.propertyPhotos,
+    this.propertyVideos,
+    this.propertyAmenities,
+    this.commercialProperty,
+    this.residentialProperty,
   });
+
+  
 
   @override
   List<Object?> get props => [
@@ -55,17 +62,22 @@ class ListingDetailEntity extends Equatable {
         title,
         description,
         propertyType,
+        propertySubType,
+        publishStatus,
+        marketStatus,
         listingType,
-        propertyPublishStatus,
-        propertyMarketStatus,
         propertySize,
         availableStartDate,
         availableEndDate,
         lister,
         paymentInformation,
         propertyLocation,
-        propertySubType,
         propertyPhotos,
         propertyVideos,
+        propertyAmenities,
+        commercialProperty,
+        residentialProperty,
       ];
+
+  
 }
