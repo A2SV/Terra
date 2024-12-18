598efe9da51ad7676779ded024c97ef8ef128cf7
Kassimmm <102239561+Kassimmm@users.noreply.github.com>
2024-12-18 08:53:31 +0000

    Ggpp 290 single listing update functionality (#253)
    * feat(web): included all necesary features in form data
    
    * feat(web): complete enum mappings
    
    * feat(web): complete implementation of add-listing functionality
    
    * feat(web): completed add-listing-functionality
    
    * feat(web): fix error message to give detailed response
    
    * fix(web): fixed google maps
    
    * fix(web): fixed map
    
    * feat(web): included all necesary features in form data
    
    * feat(web): complete enum mappings
    
    * feat(web): complete implementation of add-listing functionality
    
    * feat(web): completed add-listing-functionality
    
    * feat(web): fix error message to give detailed response
    
    * fix(web): fixed google maps
    
    * fix(web): fixed map

--- web/components/AddListing/GoogleMapComponent.tsx
+++ web/components/AddListing/GoogleMapComponent.tsx
@@ -3,16 +3,27 @@ import { GoogleMap, Marker, LoadScript } from "@react-google-maps/api";
 
 interface MapProps {
   coordinates: { lat: number; lng: number } | null;
+  onLocationSelect: (lat: number, lng: number) => void;
 }
 
-const GoogleMapComponent: React.FC<MapProps> = ({ coordinates }) => {
+const GoogleMapComponent: React.FC<MapProps> = ({ coordinates, onLocationSelect }) => {
   const GOOGLE_MAP_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY;
+
+  const handleMapClick = (event: google.maps.MapMouseEvent) => {
+    if (event.latLng) {
+      const lat = event.latLng.lat();
+      const lng = event.latLng.lng();
+      onLocationSelect(lat, lng); // Pass the coordinates back to the parent component
+    }
+  };
+
   return (
     <LoadScript googleMapsApiKey={GOOGLE_MAP_API_KEY ?? ""}>
       <GoogleMap
         mapContainerStyle={{ width: "100%", height: "400px" }}
         center={coordinates || { lat: 5.6037, lng: -0.187 }}
         zoom={coordinates ? 15 : 10}
+        onClick={handleMapClick}
       >
         {coordinates && <Marker position={coordinates} />}
       </GoogleMap>
